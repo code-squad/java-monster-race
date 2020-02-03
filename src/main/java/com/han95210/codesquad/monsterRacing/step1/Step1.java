9f0e15add3a5dc75187fcfc509f1e934eaf18c1f
han95210 <han95210@naver.com>
2020-02-03 21:44:35 +0900

    [생성] : 몬스터 레이스 Step1 구현
    [기능 요구사항]
    * 초간단 몬스터 경주 게임을 구현한다.
    * 주어진 횟수 동안 n 마리의 몬스터는 전진 또는 멈출 수 있다.
    * 사용자는 몇 마리의 몬스터로 몇 번의 이동을 할 것인지를 입력할 수 있어야 한다.
    * 전진하는 조건은 0에서 9까지 random 값 중 4 이상이면 전진한다.
    * 몬스터의 상태를 화면에 출력한다. 어느 시점에 출력할 것인지에 대한 제약은 없다.
    
    [프로그래밍 요구사항]
    * 메소드가 너무 많은 일을 하지 않도록 분리하기 위해 노력해 본다.
    * 들여쓰기는 가능하면 2단계 이내로 구현한다. 예를 들어 이중 루프 안에 if문을 사용한다면 3단계이다.
             - (02.03) 3단 들여쓰기가 되어 애를 먹었다. 메소드의 할 일을 적게 줌으로써 해결하였다.

---
+++ src/main/java/com/han95210/codesquad/monsterRacing/step1/Step1.java
@@ -0,0 +1,51 @@
+package com.han95210.codesquad.monsterRacing.step1;
+
+import java.util.Scanner;
+
+public class Step1 {
+
+    // 횟수가 주어졌을 때 랜덤으로 문자열에 -(dash)로 채워넣기
+    String getForwardCount(int numberAttempts) {
+
+        String dashStr = "";
+
+        for (int i = 0; i < numberAttempts; i++) {
+            int randomNum = (int) (Math.random() * 10);
+            if (randomNum >= 4) {
+                dashStr += "-";
+            }
+        }
+        return dashStr;
+    }
+
+    public static void main(String[] args) {
+        Step1 step1 = new Step1();
+
+        System.out.println("<스릴만점 건전한 몬스터 경주>");
+        System.out.println("몬스터는 모두 몇 마리인가요?");
+        Scanner scan = new Scanner(System.in);
+
+        // 몬스터 수 정하기
+        int numberOfMonster = scan.nextInt();
+        System.out.println(numberOfMonster);
+
+        // 시도 횟수 정하기
+        System.out.println("시도할 횟수는 몇 회 인가요?");
+        int numberAttempts = scan.nextInt();
+        System.out.println(numberAttempts);
+
+        // 이동 횟수 배열 만들기
+        String[] forwardCount = new String[numberOfMonster];
+
+        // 이동 횟수 배열에다가 getForwardCount 함수값 넣기
+        for (int i = 0; i < forwardCount.length; i++) {
+            forwardCount[i] = step1.getForwardCount(numberAttempts);
+        }
+
+        // forwardCount 배열의 각 원소 출력
+        System.out.println("<실행 결과>");
+        for(int i = 0; i < numberOfMonster; i++) {
+            System.out.println((i+1) + "번 몬스터 " + forwardCount[i]);
+        }
+    }
+}
