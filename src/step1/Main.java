5ef5e1076d290834715af3f95217cca1cb07b6c4
dd <>
2020-02-03 23:44:12 +0900

    Make new project & Complete step1

---
+++ src/step1/Main.java
@@ -0,0 +1,11 @@
+package step1;
+
+import java.io.IOException;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        MonsterRace monsterRace = new MonsterRace();
+        monsterRace.display();
+    }
+}
