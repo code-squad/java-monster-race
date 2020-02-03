e386510f987cb7fa0c8aa2cad6d97ed19375290b
Ever <hsik0225@gmail.com>
2020-02-03 21:26:36 +0900

    Monster race Step3
    반복문들을 함수형으로 고치려고 노력해보았습니다.

--- src/com/hsik0225/Step3.java
+++ src/com/hsik0225/Step3.java
@@ -1,102 +1,115 @@
 package com.hsik0225;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.stream.IntStream;
 
 public class Step3 {
-    Scanner scanner = new Scanner(System.in);
+    Scanner scanner;
+    Random random;
+    HashMap<String, String> MonsterInfoMap;
+    HashMap<String, String> moveLengthMap;
 
-    public Step3(){
+
+    public Step3() {
+        scanner = new Scanner(System.in);
+        random = new Random();
+        MonsterInfoMap = new HashMap<>();
+        moveLengthMap = new HashMap<>();
+
         setGame();
+
+        scanner.close();
     }
 
-    public void setGame(){
-        final int monsterCount = setMonsterCount();
-        final int challengeNumber = setChallengeNumber();
-        HashMap<String, String> infoMap = setMonsterInfoMap(monsterCount);
-        HashMap<String, String> spaceMap = setSpaceMap(monsterCount, challengeNumber);
-        String winner = checkWinner(infoMap, spaceMap);
-        startGame(infoMap, spaceMap, winner);
+    public void setGame() {
+        final int MONSTER_COUNT = setMonsterCount();
+        final int CHALLENGE_COUNT = setChallengeNumber();
+        setMonsterInfoMap(MONSTER_COUNT);
+        setMoveLengthMap(MONSTER_COUNT, CHALLENGE_COUNT);
+        changeNullValue();
+        String winner = checkWinner();
+        startGame(winner);
     }
-    public int setMonsterCount(){
+
+    public int setMonsterCount() {
         System.out.println("몬스터는 모두 몇 명인가요?");
         return Integer.parseInt(scanner.nextLine());
     }
 
-    public int setChallengeNumber(){
+    public int setChallengeNumber() {
         System.out.println("시도할 회수는 몇 회 인가요?");
         return Integer.parseInt(scanner.nextLine());
     }
 
-    public HashMap<String, String> setMonsterInfoMap(int monsterCount){
-        HashMap<String, String> infoMap = new HashMap<>();
-        int loopCount = monsterCount;
+    public void setMonsterInfoMap(int MONSTER_COUNT) {
         System.out.println("경주할 몬스터 이름과 종류를 입력하세요(쉼표(,)를 기준으로 구분");
-        while(loopCount-->0){
+
+        IntStream.range(0, MONSTER_COUNT).forEach(count -> {
             String[] inputMonsterInfo = scanner.nextLine().replace(" ", "").split(",");
-            infoMap.put(inputMonsterInfo[0], inputMonsterInfo[1]);
-        }
-        return infoMap;
+            MonsterInfoMap.put(inputMonsterInfo[0], inputMonsterInfo[1]);
+        });
     }
 
-    public HashMap<String, String> setSpaceMap(int monsterCount, int challengeNumber){
-        HashMap<String, String> spaceMap = new HashMap<>();
-        Random random = new Random();
-        String[] moveSpace = new String[monsterCount];
-        int loopCount = challengeNumber;
-        while(loopCount-->0){
-            int randomValue = random.nextInt(10);
-            System.out.println(randomValue);
-            if(randomValue>=9){
-                moveSpace[0] += moveMonster(randomValue,9,(random.nextInt(100)+1));
-                spaceMap.put("에스퍼", moveSpace[0]);
-            }
-            if(randomValue>=6){
-                moveSpace[1] += moveMonster(randomValue, 6,3);
-                spaceMap.put("비행", moveSpace[1]);
-            }
-            if(randomValue>=4){
-                moveSpace[2] += moveMonster(randomValue, 4, 1);
-                spaceMap.put("달리기", moveSpace[2]);
-            }
-        }
-        return spaceMap;
+    public void setMoveLengthMap(int MONSTER_COUNT, int CHALLENGE_COUNT) {
+        IntStream.range(0, CHALLENGE_COUNT)
+                .map(count -> random.nextInt(10))
+                .forEach(randomValue -> {
+                    putValue(MONSTER_COUNT, randomValue);
+                });
     }
 
-    public String moveMonster(int randomValue, int conditionValue, int space){
+    public void putValue(int MONSTER_COUNT, int randomValue) {
+        String[] monsterType = {"에스퍼", "비행", "달리기"};
+        int[] conditionValue = {9, 6, 4};
+        int[] range = {random.nextInt(100) + 1, 3, 1};
+
+        IntStream.range(0, MONSTER_COUNT)
+                .filter(index -> randomValue >= conditionValue[index])
+                .forEach(index -> {
+                    moveLengthMap.put(monsterType[index], moveLengthMap.get(monsterType[index]) + moveMonster(range[index]));
+                });
+    }
+
+    public String moveMonster(int range) {
         StringBuilder result = new StringBuilder();
-        IntStream.range(0,space).forEach(index -> result.append("-"));
+        IntStream.range(0, range).forEach(index -> result.append("-"));
         return result.toString();
     }
 
-    public String checkWinner(HashMap<String, String> infoMap, HashMap<String, String> spaceMap){
+    public String checkWinner() {
         int maxLength = 0;
         String winner = "";
-        for (String key : infoMap.keySet()) {
-            if(infoMap.get(key)==null){
-                continue;
-            }
-            int spaceLength = spaceMap.get(infoMap.get(key)).length();
-            if(spaceLength>maxLength){
-                maxLength=spaceLength;
+
+        for (String key : MonsterInfoMap.keySet()) {
+            int lengthLength = moveLengthMap.get(MonsterInfoMap.get(key)).length();
+
+            if (lengthLength > maxLength) {
+                maxLength = lengthLength;
                 winner = key;
             }
         }
-        System.out.println(winner);
         return winner;
     }
 
-    public void startGame(HashMap<String, String> infoMap, HashMap<String, String> spaceMap, String winner){
-        System.out.println("<경기 시작>\n");
+    public void changeNullValue() {
+        MonsterInfoMap.keySet()
+                .stream()
+                .map(MonsterInfoMap::get)
+                .filter(lengthMapKey -> moveLengthMap.get(lengthMapKey) == null)
+                .forEach(lengthMapKey -> moveLengthMap.put(lengthMapKey, "null"));
+    }
+
+    public void startGame(String winner) {
         int nullIndex = 4;
-        for (String key : infoMap.keySet()) {
-            System.out.println(key + " [ "
-                    + infoMap.get(key)
-                    + " ] : "
-                    + spaceMap.get(infoMap.get(key)).substring(nullIndex));
-        }
+
+        System.out.println("<경기 시작>\n");
+
+        MonsterInfoMap.keySet()
+                .forEach(key -> System.out.println(key + " [ " + MonsterInfoMap.get(key) + " ] : " + moveLengthMap.get(MonsterInfoMap.get(key)).substring(nullIndex)));
+
         System.out.println("축하합니다! " + winner + "가 몬스터 레이스에서 우승했습니다.");
     }
 }
