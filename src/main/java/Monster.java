553f1ac13941d88dd350563357ce3ec90b33dd86
beemiel <qnswwrn@gmail.com>
2020-02-04 18:19:33 +0900

    Monster class 수정
    name 추가  - 몬스터의 이름을 나타내는 name 추가
    type 추가 - 몬스터의 타입을 나타내는 type 추가
    run() 수정 - getRandom()이 들어가는 조건문을 메소드로 작성하여 변경
    runOrStop() 추가 - 랜덤값에 의한 출력여부 결정하고 "-" 출력

--- src/main/java/Monster.java
+++ src/main/java/Monster.java
@@ -1,19 +1,25 @@
 import java.util.Random;
 
 public class Monster {
+    private String name;
+    private String type;
 
-    private boolean getRandom(){
+    private boolean getRandom() {
         Random random = new Random();
         return random.nextInt(10) > 4;
     }
 
-    public void run(int count){
-        while (count>0){
-            if(getRandom()){
-                System.out.print("-");
-            }
+    public void run(int count) {
+        while (count > 0) {
+            runOrStop();
             count--;
         }
         System.out.println("");
+    }
+
+    public void runOrStop() {
+        if (getRandom())
+            System.out.print("-");
+
     }
 }
