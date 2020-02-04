9ed10ad4adb2d87f9ddf61b5913d3c2528ff8f6d
sedin2 <53131108+sedin2@users.noreply.github.com>
2020-02-04 11:24:51 +0900

    step1 make skeleton code
    startRacing() : racing start
    inputMonsterCount() : input total count of participating monster
    inputAttemptCount() : input total count of match
    runRacing() : run racing
    showResult() : show result

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Main m = new Main();
+        m.startRacing();
+        m.inputMonsterCount();
+        m.inputAttemptCount();
+        m.runRacing();
+        m.showResult();
+    }
+
+    public void startRacing() {}
+
+    public void inputMonsterCount()  { }
+
+    public void inputAttemptCount()  { }
+
+    public void runRacing() { }
+
+    public void showResult() { }
+}
