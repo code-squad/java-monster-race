13fc96bce34dc899d7532dc249e9b52d602085d8
Jay <beginin15@gmail.com>
2020-02-05 00:52:21 +0900

    Step1 코드 수정
    * .gitignore : 중복 라인 제거
    * Monster.java : 사용되지 않은 무의미한 멤버 변수 제거

--- src/main/java/Monster.java
+++ src/main/java/Monster.java
@@ -1,19 +1,19 @@
 public class Monster {
 
     private int traces;
-    private boolean isForward;
 
     public void update(){
-        boolean isForward = (int)(Math.random() * 10) >= 4 ? true : false;
+        boolean isForward = (int) (Math.random() * 10) >= 4;
 
         if(isForward)
             traces++;
     }
 
     public void print(){
+        StringBuilder buffer = new StringBuilder();
         for(int i = 0; i < this.traces; ++i){
-            System.out.print("-");
+            buffer.append("-");
         }
-        System.out.println();
+        System.out.println(buffer);
     }
 }
