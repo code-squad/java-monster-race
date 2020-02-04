2be7cbeae228a632e9a5948c26ca0646ccadc157
blossun <dnsngu@gmail.com>
2020-02-05 01:02:35 +0900

    Scanner 입력 시, 숫자 외 입력값에 대한 에러치리 추가
    InputMismatchException 에러 발생 시, 안내문구 출력 후 재입력을 받는다.

--- src/main/java/dev/solar/MonsterRace.java
+++ src/main/java/dev/solar/MonsterRace.java
@@ -1,6 +1,7 @@
 package dev.solar;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Stream;
@@ -19,14 +20,19 @@ public class MonsterRace {
     }
 
     public void inputInfo() {
-        Scanner sc = new Scanner(System.in);
-        String promptMonster = "몬스터는 모두 몇 마리인가요?";
-        String promptAttempt = "시도할 회수는 몇 회 인가요?";
+        try {
+            Scanner sc = new Scanner(System.in);
+            String promptMonster = "몬스터는 모두 몇 마리인가요?";
+            String promptAttempt = "시도할 회수는 몇 회 인가요?";
 
-        System.out.println(promptMonster);
-        monsterCount = sc.nextInt();
-        System.out.println(promptAttempt);
-        attemptCount = sc.nextInt();
+            System.out.println(promptMonster);
+            monsterCount = sc.nextInt();
+            System.out.println(promptAttempt);
+            attemptCount = sc.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("숫자 값만 입력하실 수 있습니다. 다시 입력해주세요.");
+            this.inputInfo();
+        }
     }
 
     public void addMonsters() {
@@ -43,6 +49,6 @@ public class MonsterRace {
 
     public void printResult() {
         Stream<Monster> monsterStream = monsters.stream();
-        monsterStream.forEach(monster -> System.out.println(monster.toString()));
+        monsterStream.forEach(monster -> System.out.println(monster));
     }
 }
