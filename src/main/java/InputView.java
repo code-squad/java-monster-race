ccf0792abf9379d9a181acfa516640bec0c12303
koo bohyeon <goosee115@gmail.com>
2020-02-04 11:28:00 +0900

    사용자 입력값을 받는 클래스 생성
    사용자가 게임을 세팅할 수 있도록 값을 입력받는 클래스를 작성함
    몬스터 숫자와 시도 회수를 받아올 수 있는 메서드를 따로 작성함

---
+++ src/main/java/InputView.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class InputView {
+    BufferedReader bufferedReader;
+
+    public InputView() {
+        bufferedReader = new BufferedReader(new
+                InputStreamReader(System.in));
+    }
+
+    public int monsterNumber() throws IOException {
+        System.out.println("몬스터는 모두 몇 마리인가요?");
+        return Integer.parseInt(bufferedReader.readLine());
+    }
+
+    public int tryNumber() throws IOException {
+        System.out.println("시도할 회수는 몇 회인가요?");
+        return Integer.parseInt(bufferedReader.readLine());
+    }
+}
