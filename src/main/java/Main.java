536a29b7865dbf0da7d2a52d3fbb2abbe1c722f1
hyeonjicho <huji3156@gmail.com>
2020-02-04 15:53:25 +0900

    step1 구현 - 메소드를 나눔

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,40 +2,50 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    private int numOfMonster;
-    private int numOfRound;
+    public int numOfMonster = 0;
+    public int numOfRound = 0;
 
-    public void printMonsters(int numOfMonster) {
-        for(int i = 0; i < numOfMonster; i++ ) {
-            System.out.println("Monster " + i + " has entered the race");
-        }
+    public void getUserInput() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter number of Monsters");
+        numOfMonster = scan.nextInt();
+        System.out.println("How many rounds do you want to play?");
+        numOfRound = scan.nextInt();
     }
 
-    public String printMoves(int numOfRound) {
+    public void raceIntro() {
+        Main main = new Main();
+        System.out.println(numOfMonster + " Monsters are ready to play " + numOfRound + " rounds");
+        for(int i = 0; i < numOfMonster; i++ ) {
+            System.out.println("Monster " + (i+1) + " has entered the race");
+        }
+    }
+    public void startRace() {
+        Main main = new Main();
+        System.out.println("<<<<<<<Game Start !>>>>>>>");
+        for (int i = 0; i < numOfMonster; i++ ) {
+            System.out.println("Monster" + (i+1) + " : " +  main.monsterMoves(numOfRound));
+        }
+    }
+    public String monsterMoves(int numOfRound) {
+        Main main = new Main();
         String moves = "";
-
         for(int i = 0; i < numOfRound; i++) {
             Random rand = new Random();
             int rand_int1 = rand.nextInt(10);
-            if(rand_int1 >= 4) {
-                moves += "-";
-            }
+            if(rand_int1 >= 4) moves += "-";
         } return moves;
     }
 
-    public static void main(String[] args) {
-        System.out.println("Enter number of Monsters");
-        Scanner scan = new Scanner(System.in);
-        int numOfMonster = scan.nextInt();
-        System.out.println("How many rounds do you want to play?");
-        int numOfRound = scan.nextInt();
+    public void endRace() {
+        System.out.println("Game Over");
+    }
 
-        System.out.println(numOfMonster + " Monsters ready to play " + numOfRound + " rounds");
+    public static void main(String[] args) {
         Main main = new Main();
-        main.printMonsters(numOfMonster);
-        System.out.println("<<<<<<<Game Start !>>>>>>>");
-        for (int i = 0; i < numOfMonster; i++) {
-            System.out.println("Monster" + i + main.printMoves(numOfRound));
-        }
+        main.getUserInput();
+        main.raceIntro();
+        main.startRace();
+        main.endRace();
     }
 }
