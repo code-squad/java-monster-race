4786c249ce98acccaa956954c08a87487676b1ee
David Chung <david215@berkeley.edu>
2020-02-04 20:37:50 +0900

    Implement InputHandler
    - InputHandler instance prompts users for the number of monsters and rounds.
    - Basic error handling with exception catching (input must be a positive
      integer).

--- src/main/java/InputHandler.java
+++ src/main/java/InputHandler.java
@@ -1,17 +1,37 @@
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class InputHandler {
     private BufferedReader in;
 
     public InputHandler() {
-
+        in = new BufferedReader(new InputStreamReader(System.in));
     }
 
     public int readNumMonster() {
-        return -1;
+        System.out.println("Number of monsters?");
+        return readInt();
     }
 
     public int readNumRound() {
-        return -1;
+        System.out.println("Number of rounds?");
+        return readInt();
     }
+
+    private int readInt() {
+        try {
+            int returnValue = Integer.parseInt(in.readLine());
+            if (returnValue <= 0) { throw new InvalidRangeException(); }
+            return returnValue;
+        } catch (NumberFormatException | InvalidRangeException e) {
+            System.out.println("Must be a positive integer.");
+            return readInt();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
+    private static class InvalidRangeException extends Exception {}
 }
