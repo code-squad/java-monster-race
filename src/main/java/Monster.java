f60a8e97bbebee94f6884ca7c2d43f941ab4baa3
Jay <beginin15@gmail.com>
2020-02-04 18:05:46 +0900

    Step1 Monster.java 생성
    * Monster.java : 몬스터 객체화, 매 라운드 별로 update, print 메소드를 순차적으로 호출. 추후에 몬스터의 종류가 다양해지면 해당 클래스는 추상 클래스나 인터페이스로 변경될 듯

---
+++ src/main/java/Monster.java
@@ -0,0 +1,21 @@
+package src;
+
+public class Monster {
+
+    private int traces;
+    private boolean isForward;
+
+    public void update(){
+        boolean isForward = (int)(Math.random() * 10) >= 4 ? true : false;
+
+        if(isForward)
+            traces++;
+    }
+
+    public void print(){
+        for(int i = 0; i < this.traces; ++i){
+            System.out.print("-");
+        }
+        System.out.println();
+    }
+}
