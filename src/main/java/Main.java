b7e6697ebd5aea0f1805764f84309f3aa447b29b
hyeonjicho <huji3156@gmail.com>
2020-02-04 16:47:48 +0900

    Monster 객체를 만들어서 arraylist에 넣어 줌

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public int numOfMonster = 0;
     public int numOfRound = 0;
+    List<Monster> monsters = new ArrayList<Monster>();
 
     public void getUserInput() {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter number of Monsters");
         numOfMonster = scan.nextInt();
+
+        for(int i = 0; i < numOfMonster; i++) {
+            monsters.add(new Monster(i));
+        }
+
         System.out.println("How many rounds do you want to play?");
         numOfRound = scan.nextInt();
     }
@@ -16,15 +24,16 @@ public class Main {
     public void raceIntro() {
         Main main = new Main();
         System.out.println(numOfMonster + " Monsters are ready to play " + numOfRound + " rounds");
-        for(int i = 0; i < numOfMonster; i++ ) {
-            System.out.println("Monster " + (i+1) + " has entered the race");
+        for(Monster monster : monsters) {
+            System.out.println( "Monster " + monster.index + " has entered the race");
         }
     }
     public void startRace() {
         Main main = new Main();
-        System.out.println("<<<<<<<Game Start !>>>>>>>");
+        System.out.println("<<<<<<< Game Started !>>>>>>>");
         for (int i = 0; i < numOfMonster; i++ ) {
-            System.out.println("Monster" + (i+1) + " : " +  main.monsterMoves(numOfRound));
+            System.out.println("Monster " + monsters.get(i).index + " : " + main.monsterMoves(numOfRound));
+//            System.out.println("Monster" + (i+1) + " : " +  main.monsterMoves(numOfRound));
         }
     }
     public String monsterMoves(int numOfRound) {
@@ -38,7 +47,7 @@ public class Main {
     }
 
     public void endRace() {
-        System.out.println("Game Over");
+        System.out.println("- Game Over -");
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/Monster.java
@@ -0,0 +1,10 @@
+public class Monster {
+    int index;
+    String name;
+    String vehicle;
+    int moves = 0;
+
+    public Monster(int index) {
+        this.index = index;
+    }
+}
