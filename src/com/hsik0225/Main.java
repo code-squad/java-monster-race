6acb15f628c8745d7e962a6651c593d9f3b22803
Ever <hsik0225@gmail.com>
2020-02-04 16:44:10 +0900

    Monster Race Step1
    make Skeleton Code

---
+++ src/com/hsik0225/Main.java
@@ -0,0 +1,8 @@
+package com.hsik0225;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MonsterRace monsterRace = new MonsterRace();
+    }
+}
---
+++ src/com/hsik0225/MonsterRace.java
@@ -0,0 +1,23 @@
+package com.hsik0225;
+
+public class MonsterRace {
+    public MonsterRace(){
+
+    }
+
+    public String input(){
+
+    }
+
+    public int setMONSTER_COUNT(){
+
+    }
+
+    public int setCHALLENGE_COUNT(){
+
+    }
+
+    public void startGame(){
+
+    }
+}
