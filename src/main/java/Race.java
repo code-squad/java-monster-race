5ab3bfdfabc4d0ec6dd7fe646f2891c8cc40e94c
beemiel <qnswwrn@gmail.com>
2020-02-04 18:22:09 +0900

    Race class 수정
    inputMonsterAndCount() 추가 - 사용자로부터 몬스터 수와 시도 횟수를
    입력받는 메소드 추가
    makeMonster() 추가 - 몬스터의 수 만큼 반복문을 통해 몬스터 객체를 생성하는 메소드
    setting() 추가 - inputMonsterAndCount()와 makeMonster()를 사용하여
    게임의 기본 정보를 세팅하는 메소드
    racing() 수정 - 기존에 main()에 있던 "몬스터 레이싱" 출력문의 위치를
    racing()으로 변경. setting() 추가

--- src/main/java/Race.java
+++ src/main/java/Race.java
@@ -6,21 +6,30 @@ public class Race {
     private int count;
     private ArrayList<Monster> monsters = new ArrayList<>();
 
-    public void racing(){
+    public void racing() {
+        System.out.println("몬스터 레이스");
         setting();
         System.out.println("<실행 결과>");
         monsters.forEach(monster -> monster.run(count));
     }
 
-    public void setting(){
-        System.out.println("몬스터 레이스");
+    public void setting() {
+        inputMonsterAndCount();
+        makeMonster();
+    }
 
+    public void inputMonsterAndCount(){
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("몬스터는 모두 몇 마리인가요?");
         monster = scanner.nextInt();
+
         System.out.println("시도할 회수는 몇 회 인가요?");
         count = scanner.nextInt();
-        for(int i=0; i<monster; i++){
+    }
+
+    public void makeMonster(){
+        for (int i = 0; i < monster; i++) {
             monsters.add(new Monster());
         }
     }
