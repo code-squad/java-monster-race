5eb04f84fe8763206ff822c97e3779d91b3da935
Byung Hyun Choi <choi8608@gmail.com>
2020-02-04 13:15:28 +0900

    refactor : Refactoring variable name and some method use lamda
    # Monster.java
      - public int run(roundCount)
        : 몬스터가 이동한 거리를 return 합니다.
    
    # Race.java
      _ 변수명 변경
      - 일부 lamda 를 활용하였습니다.

--- src/main/java/MonsterRace/Monster.java
+++ src/main/java/MonsterRace/Monster.java
@@ -1,4 +1,18 @@
 package MonsterRace;
 
 public class Monster {
+    /**
+     * public int run(roundCount)
+     * 몬스터가 이동한 거리를 return 합니다.
+     */
+    public int run(int roundCount) {
+        int distance = 0;
+
+        for (int i = 0; i < roundCount; i++) {
+            double cnt = Math.random() * 9;
+            distance += (cnt >= 4) ? cnt : 0;
+        }
+
+        return distance;
+    }
 }
--- src/main/java/MonsterRace/Race.java
+++ src/main/java/MonsterRace/Race.java
@@ -2,19 +2,16 @@ package MonsterRace;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
 
 public class Race {
-    /**
-     * The Round cnt : 총 라운드 수
-     * The Monsters : 몬스터의 정보가 담긴 Set
-     */
-    int roundCnt;
-    Set<Monster> monstersNum;
+    int roundCount;
+    List<Monster> monsters;
 
     public Race() {
-        monstersNum = new HashSet<>();
+        monsters = new ArrayList<Monster>();
     }
 
     /**
@@ -23,20 +20,15 @@ public class Race {
      */
     public void startRace() {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int input;
 
         try {
             System.out.println("<스릴만점 건전한 몬스터 경주>");
-
             System.out.println("몬스터는 모두 몇 마리인가요?");
-            input = Integer.parseInt(br.readLine());
-
-            for (int i = 0; i < input; i++) {
-                monstersNum.add(new Monster());
-            }
+            IntStream.range(0, Integer.parseInt(br.readLine()))
+                    .forEach(i -> monsters.add(new Monster()));
 
             System.out.println("시도할 회수는 몇 회 인가요?");
-            this.roundCnt = Integer.parseInt(br.readLine());
+            this.roundCount = Integer.parseInt(br.readLine());
 
             br.close();
         } catch (Exception e) {
@@ -45,32 +37,16 @@ public class Race {
     }
 
     /**
-     * private int movedDistance()
-     * 몬스터가 이동한 거리를 return 합니다.
-     */
-    private int movedDistance() {
-        int distance = 0;
-
-        for (int i = 0; i < roundCnt; i++) {
-            double cnt = Math.random() * 9;
-            distance += (cnt >= 4) ? cnt : 0;
-        }
-
-        return distance;
-    }
-
-    /**
      * public void resultsOfMatch()
-     * movedDistance() 의 return 값을 기반으로 이동한 거리를 출력해줍니다.
+     * monster.run() 을 호출하여 distance 를 구하고 그만큼 "-" 를 출력해줍니다.
      */
     public void resultsOfMatch() {
         System.out.println("\n<실행 결과>");
 
-        for (int i = 0; i < monstersNum.size(); i++) {
-            for (int j = 0; j < movedDistance(); j++) {
-                System.out.print("-");
-            }
+        monsters.stream().forEach(monster -> {
+            int distance = monster.run(roundCount);
+            IntStream.range(0, distance).forEach(i -> System.out.print("-"));
             System.out.println();
-        }
+        });
     }
 }
