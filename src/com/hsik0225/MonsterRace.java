3bbb66dc69397c3a284c2afcc4bc22a449ed656a
Ever <hsik0225@gmail.com>
2020-02-04 16:55:20 +0900

    Monster Race Step1
    Input 값 구현
    숫자가 아니면 다시 입력하게 하기 위해 정규표현식을 이용하였다

--- src/com/hsik0225/MonsterRace.java
+++ src/com/hsik0225/MonsterRace.java
@@ -1,20 +1,26 @@
 package com.hsik0225;
 
+import java.util.Scanner;
+
 public class MonsterRace {
     public MonsterRace(){
 
     }
 
-    public String input(){
-
+    public void setGame(){
+        final int MONSTER_COUNT = input("몬스터는 모두 몇 마리인가요?");
+        final int CHALLENGE_COUNT = input("시도할 회수는 몇 회 인가요?");
     }
 
-    public int setMONSTER_COUNT(){
-
-    }
-
-    public int setCHALLENGE_COUNT(){
-
+    public int input(String question){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(question);
+        String inputLine = scanner.nextLine();
+        while(!inputLine.matches("[\\d]*")){
+            System.out.println("숫자를 입력해주세요");
+            inputLine = scanner.nextLine();
+        }
+        return Integer.parseInt(inputLine);
     }
 
     public void startGame(){
