96bdcc7d4b23ae3732b63fba54e794c6ecaacd84
Sosah92 <thstkd92@gmail.com>
2020-02-05 16:19:44 +0900

    몬스터 레이스 스켈레톤 코드 작성
    몬스터레이스 간단한 출력부와 TODO를 작성하여 앞으로 해야할 작업에
    대해서 간단히 코드 작성했습니다.

--- src/codesquad/sunny/race/MonsterRaceMain.java
+++ src/codesquad/sunny/race/MonsterRaceMain.java
@@ -3,6 +3,14 @@ package codesquad.sunny.race;
 public class MonsterRaceMain {
 
     public static void main(String[] args) {
+        System.out.println("<스릴만점 건전한 몬스터 경주>");
+        System.out.println("몬스터는 모두 몇 마리인가요?");
+        // TODO: 2020-02-05 몬스터 숫자 입출력
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        // TODO: 2020-02-05 시도할 횟수 입출력
+        System.out.println("실행결과");
+
 
     }
+
 }
