15c858ee412afd235f7583b9014b33ac06efcfe9
Hoyoung Jung <honux77@gmail.com>
2020-02-05 16:20:11 +0900

    Revert "몬스터 레이스 Step1 구현"

--- src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-import java.io.IOException;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        MonsterMain monsterMain = new MonsterMain();
-        monsterMain.start();
-    }
-}
