43d8dcf65b4d778c625bb5a695c48608b1ad73fc
kyungrae <rlarudfo93@gmail.com>
2020-02-05 18:46:11 +0900

    feat: main 함수 작성

--- src/main/java/game/RacingMain.java
+++ src/main/java/game/RacingMain.java
@@ -2,6 +2,12 @@ package game;
 
 public class RacingMain {
     public static void main(String[] args) {
+        MonsterModel monsterModel = new MonsterModel();
+        GameController gameController = new GameController(monsterModel);
 
+        gameController.getMonsterCount();
+        gameController.getRoundCount();
+        gameController.startRacing();
+        gameController.printResult();
     }
 }
