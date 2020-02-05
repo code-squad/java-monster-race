a9b7aa3ccc5caf5912b6f88c960fcfe77768b88d
suhyunsim <cnc1106@naver.com>
2020-02-05 20:15:09 +0900

    MonsterRaceGame 메인 클래스 구현
    InputHandler에서 입력받은 몬스터 수와 전진 시도 회수를 변수에 담아준다.

---
+++ src/main/java/MonsterRaceGame.java
@@ -0,0 +1,10 @@
+public class MonsterRaceGame {
+    public static void main(String[] args) {
+        System.out.println("<스릴만점 건전한 몬스터 경주>");
+
+        InputHandler inputHandler = new InputHandler();
+        int trialNumber = inputHandler.getTrialNumber();
+        int monsterNumber = inputHandler.getMonsterNumber();
+
+    }
+}
