ed76947c65b51f54acbef621b2ce0293ed1d8a20
MuseopKim <49878687+MuseopKim@users.noreply.github.com>
2020-02-05 16:26:46 +0900

    Modify RacingGame class
    RacingGame class 를 수정 하였습니다.
     - GameMachine의 main 메서드에 더욱 간단하고 명시적인 로직을 나타내기 위하여 유저의 입력과 결과를 출력하는 메서드를 추가하였습니다.

--- src/race/monster/ragdoll/RacingGame.java
+++ src/race/monster/ragdoll/RacingGame.java
@@ -1,29 +1,41 @@
 package race.monster.ragdoll;
 
+import java.util.Scanner;
+
 public class RacingGame {
     private int numOfMonsters;
     private int numOfTries;
-    Monster[] monsters;
+    private Monster[] monsters;
+    Scanner scanner = new Scanner(System.in);
 
-    RacingGame(int numOfMonsters, int numOfTries) {
-        this.numOfMonsters = numOfMonsters;
-        this.numOfTries = numOfTries;
-        this.monsters = new Monster[numOfMonsters];
-    }
+    public RacingGame() {}
 
-    void createMonsters() {
+    void createMonster() {
         for (int i = 0; i < numOfMonsters; i++) {
             monsters[i] = new Monster();
         }
     }
 
-    void runMonsters() {
+    void startGame() {
         for (int i = 0; i < numOfMonsters; i++) {
-            monsters[i].countMove(numOfTries);
+            monsters[i].run(numOfTries);
         }
     }
 
+    void getNumOfMonster() {
+        System.out.println("<스릴만점 건전한 몬스터 경주>");
+        System.out.println("몬스터는 모두 몇 마리인가요?");
+        numOfMonsters = Integer.parseInt(scanner.nextLine());
+        monsters = new Monster[numOfMonsters];
+    }
+
+    void getNumOfTries() {
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        numOfTries = Integer.parseInt(scanner.nextLine());
+    }
+
     void printResult() {
+        System.out.println("<실행 결과>");
         for (int i = 0; i < numOfMonsters; i++) {
             System.out.println(monsters[i].toString());
         }
