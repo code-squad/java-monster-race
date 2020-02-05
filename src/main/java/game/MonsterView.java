7594c52a3d5762a7fd8332218514bbf72b1fdc57
kyungrae <rlarudfo93@gmail.com>
2020-02-05 17:13:08 +0900

    feat: 입력에 맞춰 기능을 수행하는 GameController 함수 구현
    컨트롤러는 사용자 입력에 따라 적절한 처리 함수를 호출하는 역할을 한다. 이를 위해 사용자 입력을 받아 적절한 기능을 수행하도록 구현했다.
    
    정수를 입력받는 일이 많을것 같아 정수를 입력 받는 함수를 따로 정의했다.
    
    view 클래스의 경우 인스턴스 없이 static 함수로 처리 가능할 것 같아 변경했다.

--- src/main/java/game/MonsterView.java
+++ src/main/java/game/MonsterView.java
@@ -4,7 +4,7 @@ package game;
  * 주어진 몬스터 정보를 이용해 콘솔로 출력한다.
  */
 public class MonsterView {
-    public void printResult(Monster[] monsters) {
+    public static void printResult(Monster[] monsters) {
 
     }
 }
