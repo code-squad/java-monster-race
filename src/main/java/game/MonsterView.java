951d8f6a7b0850127b4beaa1b8f8d23373485e7e
kyungrae <rlarudfo93@gmail.com>
2020-02-05 18:39:43 +0900

    feat: 레이싱 결과를 출력하는 함수 구현
    View 클래스의 경우 상태값을 가지고 있지 않다. 이런 특징을 반영해 printResult 메소드를 static 함수로 구현해 View 인스턴스 없이 사용할 수 있도록 구현했다.

--- src/main/java/game/MonsterView.java
+++ src/main/java/game/MonsterView.java
@@ -5,6 +5,14 @@ package game;
  */
 public class MonsterView {
     public static void printResult(Monster[] monsters) {
+        for(Monster monster: monsters) {
+            StringBuilder builder = new StringBuilder();
 
+            int count = monster.getDistance();
+            for(int i=0; i<count; i++)
+                builder.append("-");
+
+            System.out.println(builder.toString());
+        }
     }
 }
