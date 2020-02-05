46c0d9491a21d6b02a9494c3a2ef4f0740cef3e1
Sosah92 <thstkd92@gmail.com>
2020-02-05 16:50:04 +0900

    makeRandomCount, conditionalForward, racePrint 작성
    makeRandomCount는 랜덤숫자 생성, conditionalForward 조건부 전진
    racePrint는 race를 출력합니다. racePrint는 이중 for문으로 이루어져
    있으며 추가적인 코드추가가 필요합니다.

--- src/codesquad/sunny/race/MonsterRaceMain.java
+++ src/codesquad/sunny/race/MonsterRaceMain.java
@@ -3,6 +3,7 @@ package codesquad.sunny.race;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Random;
 
 public class MonsterRaceMain {
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,18 +19,32 @@ public class MonsterRaceMain {
         System.out.println("시도할 회수는 몇 회 인가요?");
         tryCount = Integer.parseInt(br.readLine());
         
-        process();
-        
         System.out.println("실행결과");
         racePrint();
 
     }
 
-    private static void process() {
+    private static int makeRandomCount() {
+        Random random  = new Random();
+        return random.nextInt(10);
     }
 
     private static void racePrint() {
+        for (int monster = 0; monster < monsterCount ; monster++) {
+            for (int chance = 0; chance < tryCount ; chance++) {
+
+            }
+
+        }
     }
 
-
+    private static String conditionalForward() {
+        StringBuilder sb = new StringBuilder();
+        String foward = "";
+        if (makeRandomCount() > 3) {
+            sb.append("-");
+        }
+        foward = sb.toString();
+        return foward;
+    }
 }
