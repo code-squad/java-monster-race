02563336ae3b5d8dcebdf8bd4f287e2bf8ac8af8
Jay <beginin15@gmail.com>
2020-02-06 21:03:48 +0900

    refactor : 입력값 유효 검사 코드 추가
    * verifyRaceCondition() : 입력받은 값이 0 또는 음수인 경우, 레이스를 시작하지 않도록 검사

--- src/main/java/MonsterRace.java
+++ src/main/java/MonsterRace.java
@@ -7,29 +7,18 @@ public class MonsterRace {
     private int rounds;
     private List<Monster> monsterList = new ArrayList<>();
 
-    private void readyRace(){
-        ConsoleIO.printLine("<스릴 만점 건전한 몬스터 경주>");
+    public boolean readyRace(){
+        ConsoleIO.printLine("<스릴만점 건전한 몬스터 경주>");
         setMonsters();
         setRounds();
+
+        if(verifyRaceCondition())
+            return true;
+
+        return false;
     }
 
-    private void setMonsters(){
-        ConsoleIO.printLine("출전할 몬스터는 몇 마리?");
-        int countOfMonster = ConsoleIO.getInputDataByInt();
-        initMonster(countOfMonster);
-    }
-
-    private void initMonster(int countOfMonster){
-        for(int i = 0; i < countOfMonster; ++i)
-            monsterList.add(new Monster());
-    }
-
-    private void setRounds(){
-        ConsoleIO.printLine("라운드 수?");
-        this.rounds = ConsoleIO.getInputDataByInt();
-    }
-
-    private void startRace(){
+    public void startRace(){
         ConsoleIO.printLine("<레이스 시작!>");
 
         for(int i = 1; i <= this.rounds; ++i){
@@ -38,17 +27,28 @@ public class MonsterRace {
         }
     }
 
+    private void setMonsters(){
+        int countOfMonsters = ConsoleIO.printLineReturnInt("출전할 몬스터는 몇 마리?");
+        initMonster(countOfMonsters);
+    }
+
+    private void initMonster(int countOfMonster){
+        for(int i = 0; i < countOfMonster; ++i)
+            monsterList.add(new Monster());
+    }
+
+    private void setRounds(){
+        this.rounds = ConsoleIO.printLineReturnInt("몇 라운드?");
+    }
+
+    private boolean verifyRaceCondition(){
+        return monsterList.size() > 0 && this.rounds > 0;
+    }
+
     private void updateRound(){
         for(Monster monster : monsterList){
             monster.update();
             monster.render();
         }
-    }
-
-    public static void main(String[] args) {
-        MonsterRace race = new MonsterRace();
-        race.readyRace();
-        race.startRace();
-        ConsoleIO.close();
     }
 }
