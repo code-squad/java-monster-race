52495622874634182db0a77d7937c0f7b729f4c2
blossun <dnsngu@gmail.com>
2020-02-06 17:37:39 +0900

    scanner 오픈, 종료 기능을 메서드로 분리하여 추가, 입력메서드를 nextInt() -> nextLine() 으로 변경
    사용을 다한 Scaaner을 close()를 통해 안정적으로 종료시켜 주는 것이 좋다고 하여, 프로그램 종료 전 스트림을 닫아주는 terminate() 메서드를 추가함
    그 과정에서 scanner.nextInt() 로 사용자의 입력값을 정수로 받았는데, 에러가 발생할 경우 다시 입력을 받도록 재귀호출을 하는 과정에서 스트림에 남아있는 개행문자("\n")가 남아있어서 무한으로 호출이 되어 StackOverflow 가 발생하면서 프로그램이 죽음.
    (nextInt() 메서드는 사용자의 입력값을 받아 내부적으로 개행문자이전의 숫자값만 추출해서 개행문자가 그대로 남아있다고 한다.)
    그래서 scanner.nextLine() 으로 입력받도록 수정하고, NumberFormatException 에러를 받아서 처리하도록 수정함

--- src/main/java/dev/solar/MonsterRace.java
+++ src/main/java/dev/solar/MonsterRace.java
@@ -1,37 +1,42 @@
 package dev.solar;
 
 import java.util.ArrayList;
-import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Stream;
 
 public class MonsterRace {
     List<Monster> monsters = new ArrayList<>();
+    Scanner scanner;
     int monsterCount;
     int attemptCount;
 
     public static void main(String[] args) {
         MonsterRace monsterRace = new MonsterRace();
+        monsterRace.ready();
         monsterRace.inputInfo();
         monsterRace.addMonsters();
         monsterRace.play();
         monsterRace.printResult();
+        monsterRace.terminate();
+    }
+
+    public void ready() {
+        this.scanner = new Scanner(System.in);
     }
 
     public void inputInfo() {
         try {
-            Scanner sc = new Scanner(System.in);
             String promptMonster = "몬스터는 모두 몇 마리인가요?";
             String promptAttempt = "시도할 회수는 몇 회 인가요?";
 
             System.out.println(promptMonster);
-            monsterCount = sc.nextInt();
+            monsterCount = Integer.parseInt(scanner.nextLine());
             System.out.println(promptAttempt);
-            attemptCount = sc.nextInt();
-        } catch (InputMismatchException e) {
+            attemptCount = Integer.parseInt(scanner.nextLine());
+        } catch (NumberFormatException e) {
             System.out.println("숫자 값만 입력하실 수 있습니다. 다시 입력해주세요.");
-            this.inputInfo();
+            inputInfo();
         }
     }
 
@@ -50,5 +55,9 @@ public class MonsterRace {
     public void printResult() {
         Stream<Monster> monsterStream = monsters.stream();
         monsterStream.forEach(monster -> System.out.println(monster));
+    }
+
+    public void terminate() {
+        scanner.close();
     }
 }
