5082873e5a54c6f00ddb81290036de8940bd476a
Jay <beginin15@gmail.com>
2020-02-07 14:04:17 +0900

    refactor : GameMachine 클래스의 main 메소드 수정
    * (before) MonsterRace 객체 생성 이후에 사용자의 입력이 유효하지 않으면 게임을 시작하지 않음. => (after) 사용자의 입력이 모두 유효한 경우, MonsterRace 객체를 생성하고 게임을 시작.

--- src/main/java/GameMachine.java
+++ src/main/java/GameMachine.java
@@ -1,17 +1,12 @@
 public class GameMachine {
 
-    public static void run(){
-        MonsterRace race = new MonsterRace();
-        if(race.readyRace())
-            race.startRace();
-    }
-
-    public static void close(){
-        ConsoleIO.close();
-    }
-
     public static void main(String[] args) {
-        run();
-        close();
+
+        Input.opening();
+        int countOfMonsters = Input.getCountOfMosters();
+        int rounds = Input.getRounds();
+
+        MonsterRace race = new MonsterRace(countOfMonsters, rounds);
+        race.startRace();
     }
 }
