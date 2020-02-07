ee64b9c9e1bf619c36818ea8bd8d6f0d22d33479
Ever <hsik0225@gmail.com>
2020-02-07 15:00:19 +0900

    move 구현
    Monster 클래스 Move 구현

--- src/com/hsik0225/Monster.java
+++ src/com/hsik0225/Monster.java
@@ -5,20 +5,7 @@ import java.util.Scanner;
 import java.util.stream.IntStream;
 
 public class Monster {
-    private int numOfMove;
-
-    public Monster(int numOfMove){
-        this.numOfMove = numOfMove;
-    }
-
-    public String move() {
-                .forEach(randomValue -> {
-            addLength(stringBuilder, randomValue);
-        });
-        return stringBuilder.toString();
-    }
-
-    public void addLength(StringBuilder stringBuilder, int randomValue) {
-        IntStream.range(0, randomValue).forEach(count -> stringBuilder.append("-"));
+    public String move(int numOfMove) {
+        return "-".repeat(numOfMove);
     }
 }
