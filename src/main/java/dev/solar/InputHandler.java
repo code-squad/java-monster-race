2c75e6efe199315e86a1b3c07713b75a6f64ac1d
blossun <dnsngu@gmail.com>
2020-02-07 15:54:54 +0900

    값을 입력받아 검증을 처리하는 InputHandler 클래스 생성
    사용자로부터 값을 입력받고, 입력받은 값에대한 검증을 처리하는 부분을 별도의 InputHandler 클래스로 분리

---
+++ src/main/java/dev/solar/InputHandler.java
@@ -0,0 +1,19 @@
+package dev.solar;
+
+import java.util.Scanner;
+
+public class InputHandler {
+    Scanner scanner = new Scanner(System.in);
+    private int inputValue;
+
+    public Integer inputProperType(String promptMesssage) {
+        try {
+            System.out.println(promptMesssage);
+            inputValue = Integer.parseInt(scanner.nextLine());
+            return inputValue;
+        } catch (NumberFormatException e) {
+            System.out.println("숫자 값만 입력하실 수 있습니다. 다시 입력해주세요.");
+            return inputProperType(promptMesssage);
+        }
+    }
+}
--- src/main/java/dev/solar/MonsterRace.java
+++ src/main/java/dev/solar/MonsterRace.java
@@ -2,11 +2,10 @@ package dev.solar;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 import java.util.stream.Stream;
 
 public class MonsterRace {
-    Scanner scanner;
+    private InputHandler inputHandler = new InputHandler();
     private List<Monster> monsters = new ArrayList<>();
     private int monsterCount;
     private int attemptCount;
@@ -17,11 +16,6 @@ public class MonsterRace {
         monsterRace.addMonsters();
         monsterRace.play();
         monsterRace.printResult();
-        monsterRace.terminate();
-    }
-
-    private MonsterRace() {
-        scanner = new Scanner(System.in);
     }
 
     private void inputInfo() {
@@ -31,24 +25,12 @@ public class MonsterRace {
 
     private void inputMonsterInfo() {
         String promptMonster = "몬스터는 모두 몇 마리인가요?";
-        monsterCount = inputProperType(promptMonster);
+        monsterCount = inputHandler.inputProperType(promptMonster);
     }
 
     private void inputAttemptInfo() {
         String promptMonster = "시도할 회수는 몇 회 인가요?";
-        attemptCount = inputProperType(promptMonster);
-    }
-
-    private Integer inputProperType(String promptMesssage) {
-        int inputValue;
-        try {
-            System.out.println(promptMesssage);
-            inputValue = Integer.parseInt(scanner.nextLine());
-            return inputValue;
-        } catch (NumberFormatException e) {
-            System.out.println("숫자 값만 입력하실 수 있습니다. 다시 입력해주세요.");
-            return inputProperType(promptMesssage);
-        }
+        attemptCount = inputHandler.inputProperType(promptMonster);
     }
 
     private void addMonsters() {
@@ -66,9 +48,5 @@ public class MonsterRace {
     private void printResult() {
         Stream<Monster> monsterStream = monsters.stream();
         monsterStream.forEach(monster -> System.out.println(monster));
-    }
-
-    private void terminate() {
-        scanner.close();
     }
 }
