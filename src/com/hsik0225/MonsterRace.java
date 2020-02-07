c7e954f4ef858aafadc3b9789b684749c3272804
Ever <hsik0225@gmail.com>
2020-02-07 14:43:22 +0900

    Input, 몬스터 생성 구현 완료
    게임 셋팅(몬스터의 수, 시도 횟수) 구현 완료
    몬스터 생성 구현 완료

--- src/com/hsik0225/MonsterRace.java
+++ src/com/hsik0225/MonsterRace.java
@@ -1,5 +1,6 @@
 package com.hsik0225;
 
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.stream.IntStream;
@@ -7,26 +8,42 @@ import java.util.stream.IntStream;
 public class MonsterRace {
     private Input input;
     private Output output;
+    private Random random;
+    private List<Monster> monsters;
+    private int MONSTER_COUNT;
+    private int CHALLENGE_COUNT;
 
     public MonsterRace(){
         input = new Input();
         output = new Output();
+        random = new Random();
     }
 
     public void setGame(){
         output.startMonsterRace();
 
         output.askMONSTERS_COUNT();
-        final int MONSTER_COUNT = input.input();
+        this.MONSTER_COUNT = input.digit();
 
         output.askCHALLENGE_COUNT();
-        final int CHALLENGE_COUNT = input.input();
+        this.CHALLENGE_COUNT = input.digit();
+    }
+
+    private int makeRandomNumber(int bound){
+        int randomValue = random.nextInt(bound);
+        if(randomValue>=4){
+            return randomValue;
+        }
+        return 0;
+    }
+
+    public void makeMonsters(){
+        for (int index = 0; index < MONSTER_COUNT; index++) {
+            monsters.add(new Monster(makeRandomNumber(10)));
+        }
     }
 
     public void run() {
-        Monster monster = new Monster();
-
-        System.out.println("<실행 결과>");
         IntStream.range(0, monster.numOfMonster)
                 .forEach(count -> System.out.println(monster.move()));
     }
