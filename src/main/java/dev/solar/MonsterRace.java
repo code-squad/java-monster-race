c7533a12f79b5a7af84b0fd2ae9fc915a099c07b
blossun <dnsngu@gmail.com>
2020-02-07 15:44:38 +0900

    inputMonsterInfo()와 inputAttemptInfo()에 중복된 예외처리 검증 부분을 별도의 메서드로 분리
    inputMonsterInfo()와 inputAttemptInfo()에 입력값 검증을 위한 예외처리부분이 중복되어 있어서 inputProperType()에서 입력값을 검증 후 반환해주도록 따로 분리하였다.

--- src/main/java/dev/solar/MonsterRace.java
+++ src/main/java/dev/solar/MonsterRace.java
@@ -20,7 +20,7 @@ public class MonsterRace {
         monsterRace.terminate();
     }
 
-    public MonsterRace() {
+    private MonsterRace() {
         scanner = new Scanner(System.in);
     }
 
@@ -30,26 +30,24 @@ public class MonsterRace {
     }
 
     private void inputMonsterInfo() {
-        try {
-            String promptMonster = "몬스터는 모두 몇 마리인가요?";
-
-            System.out.println(promptMonster);
-            monsterCount = Integer.parseInt(scanner.nextLine());
-        } catch (NumberFormatException e) {
-            System.out.println("숫자 값만 입력하실 수 있습니다. 다시 입력해주세요.");
-            inputMonsterInfo();
-        }
+        String promptMonster = "몬스터는 모두 몇 마리인가요?";
+        monsterCount = inputProperType(promptMonster);
     }
 
     private void inputAttemptInfo() {
-        try {
-            String promptMonster = "시도할 회수는 몇 회 인가요?";
+        String promptMonster = "시도할 회수는 몇 회 인가요?";
+        attemptCount = inputProperType(promptMonster);
+    }
 
-            System.out.println(promptMonster);
-            attemptCount = Integer.parseInt(scanner.nextLine());
+    private Integer inputProperType(String promptMesssage) {
+        int inputValue;
+        try {
+            System.out.println(promptMesssage);
+            inputValue = Integer.parseInt(scanner.nextLine());
+            return inputValue;
         } catch (NumberFormatException e) {
             System.out.println("숫자 값만 입력하실 수 있습니다. 다시 입력해주세요.");
-            inputAttemptInfo();
+            return inputProperType(promptMesssage);
         }
     }
 
