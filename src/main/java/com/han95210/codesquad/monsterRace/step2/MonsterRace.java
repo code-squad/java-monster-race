667b3be5ee8c716a25dc8c4ab4b2df2b81ce8d2d
han95210 <han95210@naver.com>
2020-02-07 17:36:48 +0900

    [몬스터 경주 Step1] Step2 브랜치를 만들고 Step2 진행을 위한 초석 다지기
    Step1 에서 제출한 소스 코드 피드백을 바탕으로 소스 코드를 수정
    * 클래스명 변경 요청 : step1 -> MonsterRace 로 바꾸세요
    * 여러 클래스와 메소드로 분할해보세요
    * 가능하면 접근한정자를 사용해 주세요
    * 메인함수에 로직을 넣지 마세요. 클래스와 메소드로 분리하세요.

--- src/main/java/com/han95210/codesquad/monsterRace/step2/MonsterRace.java
+++ src/main/java/com/han95210/codesquad/monsterRace/step2/MonsterRace.java
@@ -4,12 +4,27 @@ import java.util.Scanner;
 
 public class MonsterRace {
 
-    // 횟수가 주어졌을 때 랜덤으로 문자열에 -(dash)로 채워넣기
-    String getForwardCount(int numberAttempts) {
+    private Scanner scanner;
 
-        return getString(numberAttempts);
+    private int getNumberOfMonster() {
+        scanner = new Scanner(System.in);
+        System.out.println("<스릴만점 건전한 몬스터 경주>");
+        System.out.println("몬스터는 모두 몇 마리인가요?");
+
+        // 몬스터 수 정하기
+        int numberOfMonster = scanner.nextInt();
+        return numberOfMonster;
     }
 
+    private int getNumberAttempts() {
+        scanner = new Scanner(System.in);
+        // 시도 횟수 정하기
+        System.out.println("시도할 횟수는 몇 회 인가요?");
+        int numberAttempts = scanner.nextInt();
+        return numberAttempts;
+    }
+
+    // 횟수가 주어졌을 때 랜덤으로 문자열에 -(dash)로 채워넣기
     public static String getString(int numberAttempts) {
         StringBuilder dashStr = new StringBuilder();
 
@@ -22,34 +37,25 @@ public class MonsterRace {
         return dashStr.toString();
     }
 
-    public static void main(String[] args) {
+    // 몬스터가 진행한 만큼 출력
+    private void getForwardCount(int numberOfMonster, int numberAttempts) {
+        for (int i = 0; i < numberOfMonster; i++) {
+            System.out.println((i+1) + "번 몬스터 : " + getString(numberAttempts));
+        }
+    }
+
+    private void start() {
         MonsterRace monsterRace = new MonsterRace();
 
-        System.out.println("<스릴만점 건전한 몬스터 경주>");
-        System.out.println("몬스터는 모두 몇 마리인가요?");
-        Scanner scan = new Scanner(System.in);
-
-        // 몬스터 수 정하기
-        int numberOfMonster = scan.nextInt();
-        System.out.println(numberOfMonster);
-
-        // 시도 횟수 정하기
-        System.out.println("시도할 횟수는 몇 회 인가요?");
-        int numberAttempts = scan.nextInt();
+        int numberOfMonset = monsterRace.getNumberOfMonster();
+        System.out.println(numberOfMonset);
+        int numberAttempts = monsterRace.getNumberAttempts();
         System.out.println(numberAttempts);
+        monsterRace.getForwardCount(numberOfMonset, numberAttempts);
+    }
 
-        // 이동 횟수 배열 만들기
-        String[] forwardCount = new String[numberOfMonster];
-
-        // 이동 횟수 배열에다가 getForwardCount 함수값 넣기
-        for (int i = 0; i < forwardCount.length; i++) {
-            forwardCount[i] = monsterRace.getForwardCount(numberAttempts);
-        }
-
-        // forwardCount 배열의 각 원소 출력
-        System.out.println("<실행 결과>");
-        for(int i = 0; i < numberOfMonster; i++) {
-            System.out.println((i+1) + "번 몬스터 " + forwardCount[i]);
-        }
+    public static void main(String[] args) {
+        MonsterRace monsterRace = new MonsterRace();
+        monsterRace.start();
     }
 }
