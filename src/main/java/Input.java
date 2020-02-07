f5aa6cdb4b879de0dc6bc67c2e0b23c0e7629611
Jay <beginin15@gmail.com>
2020-02-07 14:58:44 +0900

    fix : Scanner 객체의 close 호출 시, System.in 객체도 close 되는 문제 발생
    * 입력받을 때마다 Scanner 객체를 close 했더니 System.in 객체가 삭제되어서 NoSuchElement 에러 발생함.
    
    * Scanner 객체를 멤버 변수로 관리하고, 게임 종료될 때 close 메소드를 호출하도록 변경함. (다른 방법이 있을 것 같은데 익숙하지 않아서 가장 간단하게 해결함...)
    
    * Input 클래스가 멤버 변수를 가지고 되었으므로 정적 메소드를 인스턴스 메소드로 변경함. (인스턴스를 어차피 생성해야 한다면 굳이 정적 메소드를 써야할 필요는 없으니까)

--- src/main/java/Input.java
+++ src/main/java/Input.java
@@ -1,35 +1,38 @@
 import java.util.Scanner;
 
-public class ConsoleIO {
-    private static Scanner scanner = new Scanner(System.in);
+public class Input {
 
-    public static void printLine(CharSequence line){
-        System.out.println(line);
-    }
+    private Scanner scanner = new Scanner(System.in);
 
-    public static int print(String line){
-        printLine(line);
+    public int getCountOfMosters() {
+        System.out.println("출전할 몬스터는 몇 마리?");
         return getPositiveInt();
     }
 
-    public static int getPositiveInt(){
-        int input;
-        try{
-            input = Integer.parseInt(scanner.nextLine());
-        } catch(NumberFormatException e){
-            input = 0;
-        }
-        return input;
+    public int getRounds() {
+        System.out.println("몇 라운드?");
+        return getPositiveInt();
     }
 
-    public static void close(){
+    public void closeScanner(){
         if(scanner != null)
             scanner.close();
     }
 
-    private class InvalidRangeException extends RuntimeException{
-        public InvalidRangeException() {
+    private int getPositiveInt() {
+        int input = 0;
+        try{
+            input = Integer.parseInt(scanner.nextLine());
+            if(input <= 0)
+                throw new InvalidRangeException("유효 범위에서 벗어난 입력값입니다.");
+        } catch(NumberFormatException | InvalidRangeException e) {
+            System.out.println(e.getMessage());
+            input = getPositiveInt();
         }
+        return input;
+    }
+
+    private class InvalidRangeException extends RuntimeException {
 
         public InvalidRangeException(String message) {
             super(message);
