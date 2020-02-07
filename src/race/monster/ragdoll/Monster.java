791334575f3e63f46ed8252d3f2c34e5d958b6fb
MuseopKim <49878687+MuseopKim@users.noreply.github.com>
2020-02-07 20:02:10 +0900

    Modify Monster class
    상속을 구현하기 위해 Monster 클래스를 추상 클래스로 선언하고 추상 메서드를 정의 하였습니다.
     - move(), decideMoveOrStop()을 추상 메서드화
     - 이름, 타입, 이동거리를 변수로 선언
     - 타입의 경우 종류가 한정적이기 때문에 enum을 사용.

--- src/race/monster/ragdoll/Monster.java
+++ src/race/monster/ragdoll/Monster.java
@@ -3,31 +3,45 @@ package race.monster.ragdoll;
 import java.util.Random;
 import java.lang.StringBuilder;
 
-public class Monster {
+public abstract class Monster {
+    enum Type {
+        RUNNER("달리기"), FLYING("비행"), ESPER("에스퍼");
+
+        private final String typeName;
+
+        Type(String typeName) {
+            this.typeName = typeName;
+        }
+
+        public String getTypeName() {
+            return typeName;
+        }
+    }
+
+    private String name;
+    private Type type;
     private int moveCount;
+    private int rangeAbility;
 
     public Monster() {
         this.moveCount = 0;
     }
 
-    private boolean decideMoveOrStop() {
-        boolean decision = false;
+    public abstract boolean decideMoveOrStop();
 
-        Random random = new Random();
-        int randomNum = random.nextInt(10);
-        if (randomNum >= 4) {
-            decision = true;
-        }
+//    {
+//        boolean decision = false;
+//
+//        Random random = new Random();
+//        int randomNum = random.nextInt(10);
+//        if (randomNum >= 4) {
+//            decision = true;
+//        }
+//
+//        return decision;
+//    }
 
-        return decision;
-    }
-
-    private void move(boolean decision) {
-        if (decision) {
-            moveCount++;
-        }
-        return;
-    }
+    public abstract void move(boolean decision);
 
     void run(int numOfTries) {
         for (int i = 0; i < numOfTries; i++) {
