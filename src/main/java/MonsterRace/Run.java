78f5bd587ea1183ca84291a10fcbc6aa421c5f2c
Byung Hyun Choi <choi8608@gmail.com>
2020-02-08 22:16:23 +0900

    refactor : Make MetaUtils
    각 Monster 클래스에서 Random * 10 을 해주던 메소드를 공통으로 사용하기 위한 MetaUtils 를 정의하였습니다.

--- src/main/java/MonsterRace/Run.java
+++ src/main/java/MonsterRace/Run.java
@@ -1,18 +1,17 @@
 package MonsterRace;
 
+import java.util.stream.IntStream;
+import static MonsterRace.MetaUtils.METAUTILS;
+
 public class Run implements MoveBehavior {
 
   private final int DELIMITER = 4;
   private final int MOVE_COUNT = 1;
-  private int distance = 0;
 
   @Override
   public int getMoveCount(int roundCount) {
-    for (int i = 0; i < roundCount; i++) {
-      double randomCount = Math.random() * 10;
-      distance += (randomCount >= DELIMITER) ? MOVE_COUNT : 0;
-    }
-
-    return distance;
+    return IntStream.range(0, roundCount).reduce(0, (old, notUse) ->
+        (METAUTILS.randomDice() >= DELIMITER) ? old + MOVE_COUNT : old
+    );
   }
 }
