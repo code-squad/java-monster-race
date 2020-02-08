47c7540628c94cb0f48d8acc4fe8179b40539317
beemiel <qnswwrn@gmail.com>
2020-02-08 16:37:08 +0900

    Race <feat>: 최종 우승자를 가려내는 로직 추가
    winnerMonsters - 우승자를 담을 ArrayList
    message - 콘솔에 message를 출력
    input - 콘솔에서 사용자에게 입력을 받아 처리
    output - 콘솔에 게임의 결과를 출력
    
    racing() - 기존에 output을 message객체로 변경함
    우승자를 계산하는 메소드 추가
    makeMonster() - 입력받은 count만큼 반복문을 이용하여
    사용자로부터몬스터의
    이름과 타입을 입력 받는다. 이 정보들을 통해 몬스터 객체를 생성하여
    몬스터를 저장하는 ArrayList에 저장
    returnMonsterType() - 사용자가 입력한 타입에 따라서 다른 클래스의 객체를
    반환
    calculateResults() - 최고 기록을 가져온 다음 몬스터 리스트에 있는
    몬스터들의 기록과 대조해서 최고 기록과 같으면 우승자를 담는 winner
    리스트에 담음
    addWinnerMonster(Monster monster, int highReord) - 매개변수로 받은 최고
    기록과
    몬스터의기록을 비교하는 로직을 조건문으로 작성하여 참일 경우에는우승자
    리스트에
    해당 몬스터를 추가함
    calculateHighRecord() - 우승자를 선정하기 위해서 최고 기록을 찾는 로직
    구현
    모든 몬스터 리스트를 반복문으로 돌리면서 기존에 저장된 기록과 비교할
    몬스터의
    기록 중 큰수를 record에 저장함
    이를 모든 리스트와 모두 대조함. 그 후 최고 기록을 반환해줌.

--- src/main/java/com/beemiel/monsterrace/Race.java
+++ src/main/java/com/beemiel/monsterrace/Race.java
@@ -6,26 +6,82 @@ public class Race {
     private int monster;
     private int count;
     private ArrayList<Monster> monsters = new ArrayList<>();
-    Output output = new Output();
+    private ArrayList<Monster> winnerMonsters = new ArrayList<>();
+    private Message message = new Message();
+    private Input input = new Input();
+    private Output output = new Output();
 
+    //TODO: 더 명확하게 수정
     public void racing() {
         setting();
-        output.result();
+        message.gameStart();
+        message.result();
         monsters.forEach(monster -> monster.run(count));
-        output.gameClose();
+        output.contentOfGame(monsters);
+        calculateResults();
+        output.resultOfGame(winnerMonsters);
+        message.gameClose();
+        input.close();
     }
 
     private void setting() {
-        Input input = new Input();
         monster = input.inputMonster();
-        count = input.inputCount();
-        input.close();
         makeMonster();
+        count = input.inputCount();
     }
 
     private void makeMonster(){
+        Monster newMonster;
+        String[] info;
+        String name, type;
+
+        message.monsterInfo();
+
         for (int i = 0; i < monster; i++) {
-            monsters.add(new Monster());
+            info = input.inputMonsterInfo();
+            name = info[0];
+            type = info[1];
+            newMonster = returnMonsterType(name,type);
+            monsters.add(newMonster);
         }
     }
+
+    private Monster returnMonsterType(String name, String type){
+        if(type.contains("달리기")){
+            return new Running(name, type);
+        }
+
+        if(type.contains("비행")){
+            return new Flying(name, type);
+        }
+
+        return new Psychic(name, type);
+    }
+
+    public void calculateResults(){
+        int highRecord = calculateHighRecorde();
+
+        //TODO: 메세지 클래스는 상수만 두자..(=>근데 이미 많이해서 고치기 귀찮은데...일단...생각은... ) 예외처리해.. 이넘써봐.. 나도 무브를 int 카운트 로 바꿀까..
+        monsters.forEach(monster -> {
+            addWinnerMonster(monster, highRecord);
+        });
+    }
+
+    public void addWinnerMonster(Monster monster, int highRecord){
+        if(monster.totalMove.length() >= highRecord){
+            winnerMonsters.add(monster);
+        }
+    }
+
+    public int calculateHighRecorde(){
+        int record = 0;
+
+        for(int i=0; i<monsters.size(); i++){
+            Monster monster = monsters.get(i);
+            record = Math.max(record, monster.totalMove.length());
+        }
+
+        return record;
+    }
+
 }
