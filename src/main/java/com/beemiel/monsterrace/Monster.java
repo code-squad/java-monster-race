eef10dc50b1d84601e783bf019e2511b1f4dbbf9
beemiel <qnswwrn@gmail.com>
2020-02-08 16:20:26 +0900

    Monster<feat>: 몬스터의 이름을 얻는 get 추가
    추상메소드에 주석을 달아서 어떤 기능인지 설명한 내용 추가
    
    getName() - 게임이 끝난 후 최종 우승자를 선별할 때 우승한 몬스터의이름을
    출력하기 위해서 몬스터의 이름을 반환해주는 get메소드를 추가함

--- src/main/java/com/beemiel/monsterrace/Monster.java
+++ src/main/java/com/beemiel/monsterrace/Monster.java
@@ -1,6 +1,5 @@
 package com.beemiel.monsterrace;
 
-import javax.swing.*;
 import java.util.Random;
 
 public abstract class Monster {
@@ -8,49 +7,30 @@ public abstract class Monster {
     private String type;
     protected String totalMove;
     protected Random random;
-    protected StringBuffer stringBuffer = new StringBuffer();
+    protected StringBuffer stringBuffer;
 
     public Monster(String name, String type) {
         this.name = name;
         this.type = type;
         this.random = new Random();
+        this.totalMove = "";
     }
 
-//    public int canMove(){
-//        return random.nextInt(10);
-//    }
+    public String getName() {
+        return name;
+    }
 
+    /* 랜덤값을 받아서 각자의 조건에 맞는 값이면 true를 반환 */
     abstract public boolean isMove();
-//    {
-////        Random random = new Random();
-//        return random.nextInt(10) > 4;
-//    }
 
+    /* isMove()가 true이면 원하는 타입마다 이동 */
     abstract public void moveOrStop();
-//    public void moveOrStop() {
-//        if (isMove()){
-//            Message.printRun();
-//        }
-//    }
-
-
-//            for(int i=0; i<count; i++){
-//        moveOrStop();
-//    }
 
     public void run(int count){
         for(int i=0; i<count; i++){
             moveOrStop();
         }
     }
-//    abstract public void run(int count);
-//    {
-//        while (count > 0) {
-//            runOrStop();
-//            count--;
-//        }
-//        Message.next();
-//    }
 
     @Override
     public String toString() {
