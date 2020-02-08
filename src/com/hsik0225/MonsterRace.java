24c6b46a782604ca46e06f9e3cd1d338336de481
hsik0225 <56301069+hsik0225@users.noreply.github.com>
2020-02-08 17:42:31 +0900

    Depth 2단계에서 1단계로 수정
    요구사항을 늦게봐서 다시 수정했습니다
    setNumOfMove 함수에서 Stream을 안써도 되긴 하는데 함수 나누기가 애매해서 Stream을 사용했습니다.

--- src/com/hsik0225/MonsterRace.java
+++ src/com/hsik0225/MonsterRace.java
@@ -3,17 +3,16 @@ package com.hsik0225;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.IntStream;
 
 public class MonsterRace {
     private Input input;
     private Output output;
-    private Random random;
     private List<Monster> monsters;
 
     public MonsterRace(){
         input = new Input();
         output = new Output();
-        random = new Random();
         monsters = new ArrayList<>();
     }
 
@@ -50,13 +49,10 @@ public class MonsterRace {
     }
 
     private int setNumOfMove(int bound, int moveCondition){
-        ArrayList<Integer> moveOK = new ArrayList<>();
-        for (int index = 0; index < input.getCHALLENGE_COUNT(); index++) {
-            int randomValue = random.nextInt(bound);
-            if(randomValue>=moveCondition){
-                moveOK.add(randomValue);
-            }
-        }
-        return moveOK.size();
+        Random random = new Random();
+        return (int)IntStream.range(0, input.getCHALLENGE_COUNT())
+                .map(index -> random.nextInt(bound))
+                .filter(randomValue -> randomValue>=4)
+                .count();
     }
 }
