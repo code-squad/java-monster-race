fa934f374956f102abf8cf9144d4dfc0980268d5
MuseopKim <49878687+MuseopKim@users.noreply.github.com>
2020-02-08 20:31:35 +0900

    Add Runner class
    '달리기' 타입의 Runner 클래스를 정의하고 구현하였습니다.
     - Runner 클래스에서 decideMoveOrStop(), move() 를 구현
     - Runner : 랜덤이 4 이상일 경우 moveCount 한칸 증가

--- src/race/monster/ragdoll/Runner.java
+++ src/race/monster/ragdoll/Runner.java
@@ -1,4 +1,31 @@
 package race.monster.ragdoll;
 
-public class Runner {
+import java.util.Random;
+
+public class Runner extends Monster {
+    private final int RUNNER_RANGE = 1;
+    private final int CRITERION = 4;
+
+    public Runner(String name, String type) {
+        super(name, type);
+        rangeAbility = RUNNER_RANGE;
+    }
+
+    protected boolean decideMoveOrStop() {
+        boolean decision = false;
+        Random random = new Random();
+        int randomNum = random.nextInt(10);
+
+        if (randomNum >= CRITERION) {
+            decision = true;
+        }
+
+        return decision;
+    }
+
+    protected void move(boolean decision) {
+        if (decision) {
+            moveCount += rangeAbility;
+        }
+    }
 }
