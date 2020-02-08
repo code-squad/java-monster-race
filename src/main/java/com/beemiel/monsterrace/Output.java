98f1ec902b09f51c8d030a2689322abea9ba68ae
beemiel <qnswwrn@gmail.com>
2020-02-08 16:13:20 +0900

    Output<feat>: 게임 내용 출력으로 클래스 정의 변경
    기존에는 message들을 콘솔에 출력하는 용도로 사용하던 클래스였으나
    게임의 내용을 출력하는 클래스로 변경
    
    contentOfGame() - 몬스터들이 움직인 횟수를 콘솔창에 출력해줌
    resultOfGame() - 게임이 끝나고 우승자를 콘솔창에 출력해줌

--- src/main/java/com/beemiel/monsterrace/Output.java
+++ src/main/java/com/beemiel/monsterrace/Output.java
@@ -1,32 +1,23 @@
 package com.beemiel.monsterrace;
 
+import java.util.ArrayList;
+
 public class Output {
 
-    public void gameStart(){
-        System.out.println(Message.START);
+    public void contentOfGame(ArrayList<Monster> monsters){
+        monsters.forEach(System.out::println);
     }
 
-    public void getMonster(){
-        System.out.println(Message.MONSTER);
+    public void resultOfGame(ArrayList<Monster> winners){
+        String winner = "";
+        StringBuffer stringBuffer = new StringBuffer(winner);
+
+        for(Monster monster : winners){
+            stringBuffer.append(monster.getName()).append(", ");
+            winner = stringBuffer.toString();
+        }
+
+        System.out.println("축하합니다~ [" + winner + "] 가 레이스에서 우승하였습니다.");
     }
 
-    public void getTry(){
-        System.out.println(Message.TRY);
-    }
-
-    public void result(){
-        System.out.println(Message.RESULT);
-    }
-
-    public void gameClose(){
-        System.out.println(Message.CLOSE);
-    }
-
-    public static void printRun(){
-        System.out.print(Message.RUN);
-    }
-
-    public static void next(){
-        System.out.println("");
-    }
 }
