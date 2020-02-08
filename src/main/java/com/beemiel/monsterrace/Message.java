38a60885117382097bb4dadd047021893f86cca6
beemiel <qnswwrn@gmail.com>
2020-02-08 16:17:35 +0900

    Message <feat>: 콘솔창에 메세지들을 출력하도록 추가
    기존에 Output class에 있던 기능들을 가져와서 Message class에 추가하였음
    모두 메세지를 콘솔창에 출력하는 메소드들로 구성되어 있음

--- src/main/java/com/beemiel/monsterrace/Message.java
+++ src/main/java/com/beemiel/monsterrace/Message.java
@@ -1,10 +1,41 @@
 package com.beemiel.monsterrace;
 
 public class Message {
-    final static String START = "<MONSTER RACE>";
-    final static String MONSTER = "몬스터는 모두 몇 마리인가요?";
-    final static String TRY = "시도할 회수는 몇 회 인가요?";
-    final static String RESULT = "<결  과>";
-    final static String CLOSE = "<경기 종료>";
-    final static String RUN = "▷";
+
+    final String TITLE = "<MONSTER RACE>";
+    final String START = "\n레이스 시작";
+    final String MONSTER = "몬스터는 모두 몇 마리인가요?";
+    final String MONSTER_INFO = "\n경주할 몬스터 이름과 종류를 입력하세요 (쉼표(,)를 기준으로 구분)";
+    final String TRY = "\n시도할 회수는 몇 회 인가요?";
+    final String RESULT = "\n<결  과>";
+    final String CLOSE = "\n<경기 종료>";
+
+    public void gameTitle(){
+        System.out.println(TITLE);
+    }
+
+    public void gameStart(){
+        System.out.println(START);
+    }
+
+    public void getMonster(){
+        System.out.println(MONSTER);
+    }
+
+    public void monsterInfo(){
+        System.out.println(MONSTER_INFO);
+    }
+
+    public void getTry(){
+        System.out.println(TRY);
+    }
+
+    public void result(){
+        System.out.println(RESULT);
+    }
+
+    public void gameClose(){
+        System.out.println(CLOSE);
+    }
+
 }
