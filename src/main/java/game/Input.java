26ea1985a7c277e639fe5b5560d78e4d58f90283
kyungrae <rlarudfo93@gmail.com>
2020-02-10 15:41:19 +0900

    feat: 입력 클래스 구현
    정수를 입력 받는 상황이 많을 것 같아 정수 입력 함수를 분리했다.

--- src/main/java/game/Input.java
+++ src/main/java/game/Input.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 // 게임 진행에 필요한 정보를 사용자로부터 입력을 얻는다.
 // 유효한 정보를 얻는다.
 public class Input {
+    private final static int ERROR_CODE = -1;
     private final Scanner sc;
 
     Input() {
@@ -12,10 +13,27 @@ public class Input {
     }
 
     public int getMonsterCount() {
-        return 0;
+        System.out.println("몇 명의 몬스터가 경주하나요?");
+        return getInt();
     }
 
     public int getRoundCount() {
-        return 0;
+        System.out.println("시도할 회수는 몇회인가요?");
+        return getInt();
+    }
+
+    private int getInt() {
+        int input = ERROR_CODE;
+
+        while(input == ERROR_CODE) {
+            try {
+                input = Integer.parseInt(sc.nextLine());
+            }catch (NumberFormatException e) {
+                System.out.println("입력 형식을 확인해주세요.");
+                input = ERROR_CODE;
+            }
+        }
+
+        return input;
     }
 }
