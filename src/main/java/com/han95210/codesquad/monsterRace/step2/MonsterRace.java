4bd12c38ee8b382929b6991d5029d1e1e615d91d
han95210 <han95210@naver.com>
2020-02-10 15:37:22 +0900

    [몬스터 경주 Step2] MonsterRace Class 프로그래밍 요구사항 반영
    프로그래밍 요구사항
    * indent(인덴트, 들여쓰기) depth를 2단계 -> 1단계로 줄여라
       * depth의 경우 if문을 사용하는 경우 1단계의 depth가 증가한다.
    * eles를 사용하지 마라.
    * 메소드의 크기가 쵣대 10라인을 넘지 않도록 구현한다.
    * 메소드가 한 가지 일만 하도록 최대한 작게 만들어라.
    * stream은 가능하면 사용하지 않는다.
    * 자바 코딩 컨벤션을 지키면서 프로그래밍한다.

--- src/main/java/com/han95210/codesquad/monsterRace/step2/MonsterRace.java
+++ src/main/java/com/han95210/codesquad/monsterRace/step2/MonsterRace.java
@@ -8,31 +8,36 @@ public class MonsterRace {
 
     private int getNumberOfMonster() {
         scanner = new Scanner(System.in);
-        System.out.println("<스릴만점 건전한 몬스터 경주>");
-        System.out.println("몬스터는 모두 몇 마리인가요?");
 
         // 몬스터 수 정하기
-        int numberOfMonster = scanner.nextInt();
-        return numberOfMonster;
+        System.out.println("몬스터는 모두 몇 마리인가요?");
+        return scanner.nextInt();
     }
 
+    // 기능 1개 : 시도할 횟수 입력 받기
     private int getNumberAttempts() {
         scanner = new Scanner(System.in);
+
         // 시도 횟수 정하기
         System.out.println("시도할 횟수는 몇 회 인가요?");
-        int numberAttempts = scanner.nextInt();
-        return numberAttempts;
+        return scanner.nextInt();
+    }
+
+    private static String getRandomDash() {
+        String dashStr = "";
+        int randomNum = (int) (Math.random() * 10);
+        if (randomNum >= 4) {
+            dashStr = "-";
+        }
+        return dashStr;
     }
 
     // 횟수가 주어졌을 때 랜덤으로 문자열에 -(dash)로 채워넣기
-    public static String getString(int numberAttempts) {
+    private static String getDashStr(int numberAttempts) {
         StringBuilder dashStr = new StringBuilder();
 
         for (int i = 0; i < numberAttempts; i++) {
-            int randomNum = (int) (Math.random() * 10);
-            if (randomNum >= 4) {
-                dashStr.append("-");
-            }
+            dashStr.append(getRandomDash());
         }
         return dashStr.toString();
     }
@@ -40,18 +45,20 @@ public class MonsterRace {
     // 몬스터가 진행한 만큼 출력
     private void getForwardCount(int numberOfMonster, int numberAttempts) {
         for (int i = 0; i < numberOfMonster; i++) {
-            System.out.println((i+1) + "번 몬스터 : " + getString(numberAttempts));
+            System.out.println((i+1) + "번 몬스터 : " + getDashStr(numberAttempts));
         }
     }
 
     private void start() {
         MonsterRace monsterRace = new MonsterRace();
 
-        int numberOfMonset = monsterRace.getNumberOfMonster();
-        System.out.println(numberOfMonset);
+        System.out.println("<스릴만점 건전한 몬스터 경주>");
+
+        int numberOfMonster = monsterRace.getNumberOfMonster();
+        System.out.println(numberOfMonster);
         int numberAttempts = monsterRace.getNumberAttempts();
         System.out.println(numberAttempts);
-        monsterRace.getForwardCount(numberOfMonset, numberAttempts);
+        monsterRace.getForwardCount(numberOfMonster, numberAttempts);
     }
 
     public static void main(String[] args) {
