093b6c1386e6690300c8adcfe7793cd30c559aaf
kyungrae <rlarudfo93@gmail.com>
2020-02-10 15:47:22 +0900

    feat: 아웃풋 클래스 구현
    몬스터 리스트를 입력을 받아 결과를 출력했다.

--- src/main/java/game/GameMain.java
+++ src/main/java/game/GameMain.java
@@ -4,6 +4,7 @@ public class GameMain {
     public static void main(String[] args) {
         GameController gameController = new GameController();
 
+        System.out.println("<신나는 몬스터 레이스>");
         gameController.initialize();
         gameController.race();
         gameController.displayResult();
