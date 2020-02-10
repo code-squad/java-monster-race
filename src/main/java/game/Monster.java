7da0d8d497a44e319ee9e66bf994eba346b18a83
kyungrae <rlarudfo93@gmail.com>
2020-02-10 15:44:38 +0900

    feat: 몬스터 클래스 구현
    몬스터 이동 로직을 위해 몬스터 객체가 모두 공유할 수 있는 랜덤 객체 생성

--- src/main/java/game/Monster.java
+++ src/main/java/game/Monster.java
@@ -1,10 +1,29 @@
 package game;
 
+import java.util.Random;
+
 // 몬스터 객체의 속성인 거리를 관리한다.
 public class Monster {
-    int distance;
+    private static final Random random = new Random();
+    private int distance;
+
+    Monster() {
+        distance = 0;
+    }
 
     public void move() {
+        if(random.nextInt(10) >= 4) {
+            distance++;
+        }
+    }
 
+    String getDistance() {
+        StringBuilder buffer = new StringBuilder();
+
+        for(int i=0; i<distance; i++) {
+            buffer.append("-");
+        }
+
+        return buffer.toString();
     }
 }
