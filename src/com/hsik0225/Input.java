1bb3f8128b473a0cd0aa10ebc29ae6218be905f7
Ever <hsik0225@gmail.com>
2020-02-10 20:17:32 +0900

    정규표현식 수정
    while문 안에서 String.matches 를 돌게 구현했었다.
    이렇게 되면 matches안에 Pattern이 계속 만들어져서 코드의 낭비가
    발생한다.
    그러므로 static final Pattern을 클래스 변수로 빼서 구현하였다

--- src/com/hsik0225/Input.java
+++ src/com/hsik0225/Input.java
@@ -1,17 +1,22 @@
 package com.hsik0225;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Input {
+    private static final Pattern DIGIT_PATTERN = Pattern.compile("[1-9][0-9]*");
+    private static final Pattern MONSTER_INFO_PATTERN = Pattern.compile(".*[,]\\s*(비행|에스퍼|달리기)\\s*");
     private Scanner scanner;
 
     public Input() {
         scanner = new Scanner(System.in);
     }
 
-    private String inputLine(String regex, OutputStrings outputString){
+    private String inputLine(Pattern pattern, OutputStrings outputString){
         String inputLine = scanner.nextLine();
-        while (!inputLine.matches(regex)) {
+        Matcher matcher =  pattern.matcher(inputLine);
+        while (!matcher.matches()) {
             System.out.println(outputString);
             inputLine = scanner.nextLine();
         }
@@ -19,15 +24,13 @@ public class Input {
     }
 
     public int inputDigit() {
-        String regex = "[1-9][0-9]*";
         OutputStrings outputString = OutputStrings.INPUT_DIGIT;
-        String digit = inputLine(regex, outputString);
+        String digit = inputLine(DIGIT_PATTERN, outputString);
         return Integer.parseInt(digit);
     }
 
     public String inputMonstersInfo(){
-        String regex = ".*[,]\\s*(비행|에스퍼|달리기)\\s*";
         OutputStrings outputString = OutputStrings.INPUT_TYPE;
-        return inputLine(regex, outputString);
+        return inputLine(MONSTER_INFO_PATTERN, outputString);
     }
 }
