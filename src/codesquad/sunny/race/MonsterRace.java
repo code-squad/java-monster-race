40dfdfa9fe5097fa9fbff2cefef5ffa0b68765b3
Sosah <thstkd92@gmail.com>
2020-02-10 16:41:23 +0900

    음수, 0 예외처리 수정 및 작동확인 완료
    NegativeNumberException이라는 클래스는 만들었지만 제대로 된 활용이 어렵다.

--- src/codesquad/sunny/race/MonsterRace.java
+++ src/codesquad/sunny/race/MonsterRace.java
@@ -9,60 +9,63 @@ public class MonsterRace {
     private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private int monsterCount;
     private int attemptCount;
+    private final int count = 5;
 
-    public MonsterRace() throws IOException {
+    public MonsterRace() throws Exception {
         showTitle();
-        askMonsterQuantity();
-        askAttemptCount();
+        askMonsterQuantity(count);
+        askAttemptCount(count);
         racePrint();
         br.close();
     }
 
-    private void showTitle(){
+    private void showTitle() {
         System.out.println("<스릴만점 건전한 몬스터 경주>");
     }
 
-    private int askMonsterQuantity() {
+    private void askMonsterQuantity(int count) throws Exception {
+        if (count == 0) throw new Exception("시도횟수 초과");
         System.out.println("몬스터는 모두 몇 마리인가요?");
         try {
             monsterCount = Integer.parseInt(br.readLine());
-            if (monsterCount < 0) {
-                throw new IllegalAccessException("음수입력했습니다. 다시 입력해 주세요.");
-            }
-        } catch (IOException | IllegalAccessException e) {
-            e.printStackTrace();
+            checkNegativeCount(monsterCount);
+        } catch (IOException | NegativeNumberException e) {
+            askMonsterQuantity(count - 1);
         }
-        return monsterCount;
     }
 
-    private int askAttemptCount() {
+    private void askAttemptCount(int count) throws Exception {
+        if (count == 0) throw new Exception("시도횟수 초과");
         System.out.println("시도할 회수는 몇 회 인가요?");
         try {
             attemptCount = Integer.parseInt(br.readLine());
-            if (attemptCount < 0) {
-                throw new IllegalAccessException("음수입력했습니다. 다시 입력해 주세요.");
-            }
-        } catch (IOException | IllegalAccessException e) {
-            e.printStackTrace();
+            checkNegativeCount(attemptCount);
+        } catch (IOException | NegativeNumberException e) {
+            askAttemptCount(count - 1);
         }
-        return attemptCount;
+    }
+
+    private void checkNegativeCount(int count) throws NegativeNumberException {
+        if (count < 0 || count == 0) {
+          throw new NegativeNumberException();
+        }
     }
 
     private int makeRandomCount() {
-        Random random  = new Random();
+        Random random = new Random();
         return random.nextInt(10);
     }
 
     private void racePrint() {
         System.out.println("실행결과");
-        for (int monster = 0; monster < monsterCount ; monster++) {
+        for (int monster = 0; monster < monsterCount; monster++) {
             attemptForward();
             System.out.println();
         }
     }
 
-    private void attemptForward(){
-        for (int chance = 0; chance < attemptCount ; chance++) {
+    private void attemptForward() {
+        for (int chance = 0; chance < attemptCount; chance++) {
             System.out.print(conditionalForward());
         }
     }
