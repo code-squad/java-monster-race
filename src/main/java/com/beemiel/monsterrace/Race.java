c153f963d01b7901d9014114cd82651e81abec33
beemiel <qnswwrn@gmail.com>
2020-02-11 21:08:29 +0900

    Race <modified>: 예외처리 추가, makeMonster() 수정
    예외처리 - 사용자의 입력을 받는 부분에서 발행하는 예외를 처리하여
    제대로 입력될 때까지 반복하여 입력을 받는다.
    makeMonster() - input.inpputMonsterInfo()에서문자열 배열을 반환받지 않고
    입력한 타입에 따라 다른 monster객체를 반환받아서 리스트에 넣도록 수정함

--- src/main/java/com/beemiel/monsterrace/Race.java
+++ src/main/java/com/beemiel/monsterrace/Race.java
@@ -1,6 +1,7 @@
 package com.beemiel.monsterrace;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 
 public class Race {
     private int monster;
@@ -11,57 +12,41 @@ public class Race {
     private Input input = new Input();
     private Output output = new Output();
 
-    //TODO: 더 명확하게 수정
     public void racing() {
         setting();
-        message.gameStart();
-        message.result();
         monsters.forEach(monster -> monster.run(count));
         output.contentOfGame(monsters);
         calculateResults();
         output.resultOfGame(winnerMonsters);
-        message.gameClose();
         input.close();
     }
 
-    private void setting() {
-        monster = input.inputMonster();
-        makeMonster();
-        count = input.inputCount();
+    public void setting() {
+        while (true){
+            try{
+                monster = input.inputMonster();
+                count = input.inputCount();
+                makeMonster();
+                break;
+            } catch (NumberFormatException | InputMismatchException n){
+                System.out.println("숫자를 입력하세요.");
+            }
+        }
     }
 
-    private void makeMonster(){
+    public void makeMonster(){
         Monster newMonster;
-        String[] info;
-        String name, type;
 
         message.monsterInfo();
-
         for (int i = 0; i < monster; i++) {
-            info = input.inputMonsterInfo();
-            name = info[0];
-            type = info[1];
-            newMonster = returnMonsterType(name,type);
+            newMonster = input.inputMonsterInfo();
             monsters.add(newMonster);
         }
-    }
-
-    private Monster returnMonsterType(String name, String type){
-        if(type.contains("달리기")){
-            return new Running(name, type);
-        }
-
-        if(type.contains("비행")){
-            return new Flying(name, type);
-        }
-
-        return new Psychic(name, type);
     }
 
     public void calculateResults(){
         int highRecord = calculateHighRecorde();
 
-        //TODO: 메세지 클래스는 상수만 두자..(=>근데 이미 많이해서 고치기 귀찮은데...일단...생각은... ) 예외처리해.. 이넘써봐.. 나도 무브를 int 카운트 로 바꿀까..
         monsters.forEach(monster -> {
             addWinnerMonster(monster, highRecord);
         });
