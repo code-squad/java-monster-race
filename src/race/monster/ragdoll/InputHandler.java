9b4b6e64a189b90b76b784b87c1137e9c030bee2
MuseopKim <49878687+MuseopKim@users.noreply.github.com>
2020-02-11 15:18:32 +0900

    Add some exception handling
    일부 입력 값에 대해 예외 처리를 추가 하였습니다.
     - 숫자가 입력 되어야 할 부분에 다른 타입의 값이 입력 되거나 음수가 입력 될 경우 예외처리
     - 입력된 값이 몬스터 타입과 일치하지 않는 값일 경우 예외 처리

--- src/race/monster/ragdoll/InputHandler.java
+++ src/race/monster/ragdoll/InputHandler.java
@@ -9,22 +9,24 @@ public class InputHandler {
 
     Scanner scanner = new Scanner(System.in);
 
-    public InputHandler() {
-    }
+    int setNumOfMonsters() {
+        try {
+            System.out.println("<스릴만점 건전한 몬스터 경주>");
+            System.out.println("몬스터는 모두 몇 마리인가요?");
+            int inputNumOfMonsters = Integer.parseInt(scanner.nextLine());
 
-    int setNumOfMonsters() throws IOException {
-        System.out.println("<스릴만점 건전한 몬스터 경주>");
-        System.out.println("몬스터는 모두 몇 마리인가요?");
-        int inputNumOfMonsters = Integer.parseInt(scanner.nextLine());
+            if (inputNumOfMonsters <= 0) {
+                throw new IOException("자연수가 입력되지 않았습니다..");
+            }
 
-        if (inputNumOfMonsters < 0) {
-            throw new IOException("음수가 입력 되었습니다.");
+            return inputNumOfMonsters;
+        } catch (IOException | NumberFormatException e) {
+            System.out.println("자연수를 입력 해주세요.");
+            return setNumOfMonsters();
         }
-
-        return inputNumOfMonsters;
     }
 
-    private String[] splitProperty() {
+    private String[] splitProperties() {
         String inputText = scanner.nextLine();
         String blankDeletedText = inputText.replaceAll(" ", "");
 
@@ -32,24 +34,29 @@ public class InputHandler {
     }
 
     void setMonsterProperties() {
-        final String RUNNER = "달리기", FLYING = "비행", ESPER = "에스퍼";
-        String[] properties = splitProperty();
+        try {
+            final String RUNNER = "달리기", FLYING = "비행", ESPER = "에스퍼";
+            String[] properties = splitProperties();
 
-        inputNameOfMonster = properties[0];
-        String typeNameOfMonster = properties[1];
+            inputNameOfMonster = properties[0];
+            String typeNameOfMonster = properties[1];
 
-        switch (typeNameOfMonster) {
-            case RUNNER:
-                inputTypeOfMonster = Types.RUNNER;
-                break;
-            case FLYING:
-                inputTypeOfMonster = Types.FLYING;
-                break;
-            case ESPER:
-                inputTypeOfMonster = Types.ESPER;
-                break;
-            default:
-                throw new IllegalArgumentException("잘못된 타입이 입력 되었습니다.");
+            switch (typeNameOfMonster) {
+                case RUNNER:
+                    inputTypeOfMonster = Types.RUNNER;
+                    break;
+                case FLYING:
+                    inputTypeOfMonster = Types.FLYING;
+                    break;
+                case ESPER:
+                    inputTypeOfMonster = Types.ESPER;
+                    break;
+                default:
+                    throw new IllegalArgumentException("잘못된 타입이 입력 되었습니다.");
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println("'달리기', '비행', '에스퍼'중 한가지 타입을 입력 해주세요.");
+            setMonsterProperties();
         }
     }
 
@@ -62,9 +69,18 @@ public class InputHandler {
     }
 
     int setNumOfTries() {
-        System.out.println("시도할 회수는 몇 회 인가요?");
-        int inputNumOfTries = Integer.parseInt(scanner.nextLine());
+        try {
+            System.out.println("시도할 회수는 몇 회 인가요?");
+            int inputNumOfTries = Integer.parseInt(scanner.nextLine());
 
-        return inputNumOfTries;
+            if (inputNumOfTries <= 0) {
+                throw new IOException("자연수가 입력되지 않았습니다.");
+            }
+            return inputNumOfTries;
+        } catch (IOException e) {
+            System.out.println("자연수를 입력 해주세요.");
+            return setNumOfTries();
+        }
+
     }
 }
