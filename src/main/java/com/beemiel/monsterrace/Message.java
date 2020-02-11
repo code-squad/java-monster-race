439dfc173c1550ab81af75cf312d12d528756dec
beemiel <qnswwrn@gmail.com>
2020-02-11 19:07:00 +0900

    Message <modified>: MONSTER_INFO 문자열 수정
    기존의 문자열에서 예시를 예> 크롱,FLY 로 변경

--- src/main/java/com/beemiel/monsterrace/Message.java
+++ src/main/java/com/beemiel/monsterrace/Message.java
@@ -5,7 +5,7 @@ public class Message {
     final String TITLE = "<MONSTER RACE>";
     final String START = "\n레이스 시작";
     final String MONSTER = "몬스터는 모두 몇 마리인가요?";
-    final String MONSTER_INFO = "\n경주할 몬스터 이름과 종류를 입력하세요 (쉼표(,)를 기준으로 구분)";
+    final String MONSTER_INFO = "\n경주할 몬스터 이름과 종류를 입력하세요 (예> 크롱,FLY)";
     final String TRY = "\n시도할 회수는 몇 회 인가요?";
     final String RESULT = "\n<결  과>";
     final String CLOSE = "\n<경기 종료>";
