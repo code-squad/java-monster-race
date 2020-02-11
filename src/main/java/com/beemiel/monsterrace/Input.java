bee4707ffe721b11f236d444f02c5249739e0afc
beemiel <qnswwrn@gmail.com>
2020-02-11 21:18:02 +0900

    Input <modified>: 예외 던지기 추가, inputMonsterInfo() 수정
    inputMonster() - NumberFormatException 을 던져주도록 추가
    inputCount() - InputMismatchException을 던져주도록 추가
    inputMonsterInfo() - 입력받은 값을 통으로 enum.valueOf()를 사용하여
    enum에 선언된 타입일 경우, 해당 타입의 enum을 반환한 후 name을
    저장해준 다음
    해당 객체를 getMonster()로 반환받아서 다시 반환해줌.
    입력한 타입이 존재하지 않을 경우에는 예외를 반환하여 알림 메세지를
    출력해주고
    다시 입력하도록 반복문을 돔

--- src/main/java/com/beemiel/monsterrace/Input.java
+++ src/main/java/com/beemiel/monsterrace/Input.java
@@ -1,22 +1,37 @@
 package com.beemiel.monsterrace;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Input {
     private Scanner scanner = new Scanner(System.in);
     private Message message = new Message();
 
-    public int inputMonster(){
+    public int inputMonster() throws NumberFormatException{
         message.gameTitle();
         message.getMonster();
         return Integer.parseInt(scanner.nextLine());
     }
 
-    public String[] inputMonsterInfo(){
-        return scanner.nextLine().split(",");
+    public Monster inputMonsterInfo() {
+        String[] info;
+
+        while(true){
+            try{
+                info = scanner.nextLine().split(",");
+                String type = info[1];
+                String name = info[0];
+                MonsterType monsterType = MonsterType.valueOf(type);
+                monsterType.getMonster().setName(name);
+                return monsterType.getMonster();
+            } catch (Exception e){
+                e.getMessage();
+                System.out.println("제대로 된 타입을 입력하세요");
+            }
+        }
     }
 
-    public int inputCount(){
+    public int inputCount() throws InputMismatchException {
         message.getTry();
         return scanner.nextInt();
     }
