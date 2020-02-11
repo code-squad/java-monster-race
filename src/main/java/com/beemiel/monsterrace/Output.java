6edbd4f688c8d94c887aa1d0b362b9a1100cd92b
beemiel <qnswwrn@gmail.com>
2020-02-11 21:13:58 +0900

    Output <modified>: 출력 메세지 추가, StringJoiner 사용
    외부에 있던 출력문을 Output클래스 안으로 가져옴
    resultOfGame() - 기존에 StringBuffer를 사용하던 부분을 StringJoiner로
    변경
    앞뒤와 중간에 알아서 문자를 붙여줘서 코드가 더 간결하고 편리해짐

--- src/main/java/com/beemiel/monsterrace/Output.java
+++ src/main/java/com/beemiel/monsterrace/Output.java
@@ -1,23 +1,24 @@
 package com.beemiel.monsterrace;
 
 import java.util.ArrayList;
+import java.util.StringJoiner;
 
 public class Output {
+    private Message message = new Message();
 
     public void contentOfGame(ArrayList<Monster> monsters){
+        message.gameStart();
+        message.result();
         monsters.forEach(System.out::println);
     }
 
     public void resultOfGame(ArrayList<Monster> winners){
-        String winner = "";
-        StringBuffer stringBuffer = new StringBuffer(winner);
-
+        StringJoiner stringJoiner = new StringJoiner(",","[","]");
         for(Monster monster : winners){
-            stringBuffer.append(monster.getName()).append(", ");
-            winner = stringBuffer.toString();
+            stringJoiner.add(monster.getName());
         }
-
-        System.out.println("축하합니다~ [" + winner + "] 가 레이스에서 우승하였습니다.");
+        System.out.println("축하합니다~ " + stringJoiner.toString() + " 가 레이스에서 우승하였습니다.");
+        message.gameClose();
     }
 
 }
