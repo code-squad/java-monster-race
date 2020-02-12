07ce1eaf6dcf77a21e6946443f8dc789c50af547
kyungrae <rlarudfo93@gmail.com>
2020-02-12 21:26:02 +0900

    refactor: 불필요한 상태값과 private method 제거
    필요없는 상태값을 보관할 필요 없는 것 같다.

--- src/main/java/game/Race.java
+++ src/main/java/game/Race.java
@@ -1,36 +1,25 @@
 package game;
 
-import java.util.ArrayList;
 import java.util.List;
 
 // 게임 전체 로직을 진행하는 역할
 public class Race {
     private Rule rule = new Rule();
-    private List<Monster> participants = new ArrayList<>();
 
     // 게임을 시작한다.
     public void start() {
-        setRule();
-        enterParticipants();
+        rule.setRule();
+        List<Monster> participants = Reception.registerParticipants(rule.getTotalParticipant());
 
         System.out.println("\n경기를 시작합니다.");
         for(int round = 0; round<rule.getTotalRound(); round++) {
-            playRound();
+            playRound(participants);
             Dashboard.displayCurrent(round, participants);
         }
         Dashboard.displayResult(participants);
     }
 
-    private void setRule() {
-        rule.setRule();
-    }
-
-    private void enterParticipants() {
-        participants.clear();
-        participants.addAll(Reception.registerParticipants(rule.getTotalParticipant()));
-    }
-
-    private void playRound() {
+    private void playRound(List<Monster> participants) {
         for(Monster participant: participants) {
             participant.move();
         }
