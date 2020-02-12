c0127e02cb88367a1d60c48ab8033d5deeab5d42
kyungrae <rlarudfo93@gmail.com>
2020-02-12 11:23:22 +0900

    refactor: GameController->Race, GameMain->RaceMain 수정
    GameController 클래스 이름이 만족스럽지 못하다. 리뷰처럼 사용자의 입력에 맞게 게임을 컨트롤하는 느낌이 있다. Object 책에 내용 처럼 객체를 능동적으로 활동할 수 있는 주체로 생각하고 Race로 바꾸었다.
    
    Race는 단발성 게임이니 굳이 monsterList를 상태값으로 갖을 필요가 없어 보여 제거했다. 대신에 게임을 시작할 때, 만들어서 지역변수로 이용했다.

--- src/main/java/game/Dashboard.java
+++ src/main/java/game/Dashboard.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 // 경기 정보를 표준 출력을 통해 출력하는 책임이 있다.
 public class Dashboard {
-    public static void displayResult(List<Monster> monsters) {
-        System.out.println();
+    public static void displayResult(int round, List<Monster> monsters) {
+        System.out.printf("\n----%d----\n", round);
         for(Monster monster: monsters){
             System.out.println(monster.getDistance());
         }
