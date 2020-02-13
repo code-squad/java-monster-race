61d6b6ded293881dde40976ea6141d0bc65771bd
beemiel <qnswwrn@gmail.com>
2020-02-13 23:42:05 +0900

    MonsterType<수정>: 상수 재정의, 값 찾는 메서드,몬스터 만드는 메서드 추가
    enum을 잘못 사용하여 수정
    기존의 상수들이 가지고 있던 값이 객체였어서 적절한 값으로 변경
    findTypeByInput(String input) - 상수 중 매개변수와 동일한 값을 갖고 있는
    상수를
    반환해준다. 만약 없다면 예외를 반환해줌
    makeMonsterByType(String name) - enum객체가 갖고 있는 값에 따라서 다른
    타입의
    객체를 반환한다.

--- src/main/java/com/beemiel/monsterrace/MonsterType.java
+++ src/main/java/com/beemiel/monsterrace/MonsterType.java
@@ -1,16 +1,41 @@
 package com.beemiel.monsterrace;
 
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
 public enum MonsterType {
-    FLY(new Flying("비행")), RUN(new Running("달리기")), ESPER(new Psychic("에스퍼"));
+    FLY("비행"), RUN("달리기"), ESPER("에스퍼");
 
     //연관된 데이터
-    Monster monster;
+    String type;
 
-    MonsterType(Monster monster) {
-        this.monster = monster;
+    MonsterType(String type) {
+        this.type = type;
     }
 
-    public Monster getMonster(){
-        return monster;
+    public String getType() {
+        return type;
+    }
+
+    public static MonsterType findTypeByInput(String input){
+        return Arrays.stream(MonsterType.values())
+                .filter(monsterType -> input.equals(monsterType.getType()))
+                .findAny()
+                .orElseThrow(() -> new NoSuchElementException("존재하는 타입이 아닙니다."));
+    }
+
+    public Monster makeMonsterByType(String name){
+        if(type.equals("비행")){
+            System.out.println("비행 타입 생성");
+            return new Flying(type,name);
+        }
+
+        if(type.equals("달리기")){
+            System.out.println("달리기 타입 생성");
+            return new Running(type,name);
+        }
+
+        System.out.println("에스퍼 타입 생성");
+        return new Psychic(type,name);
     }
 }
