d571ef82384635c460973bcb61cb5478731c88b4
beemiel <qnswwrn@gmail.com>
2020-02-14 16:21:08 +0900

    Race<수정>: 경기 결과를 계산하는 메서드를  다른 클래스로 분리
    기존에 경기 결과를 계산하여 최종 우승자를 찾는 메서드들을 Calculator라는
    클래스로 분리함
    
    startRacing() - 실제 게임내용을 연산하는 메서드

--- src/main/java/com/beemiel/monsterrace/Race.java
+++ src/main/java/com/beemiel/monsterrace/Race.java
@@ -1,72 +1,26 @@
 package com.beemiel.monsterrace;
 
 import java.util.ArrayList;
-import java.util.InputMismatchException;
 
 public class Race {
-    private int monster;
-    private int count;
     private ArrayList<Monster> monsters = new ArrayList<>();
-    private ArrayList<Monster> winnerMonsters = new ArrayList<>();
-    private Message message = new Message();
-    private Input input = new Input();
-    private Output output = new Output();
 
     public void racing() {
-        setting();
+        Setting setting = new Setting();
+        Result result = new Result();
+        Calculator calculator = new Calculator();
+
+        setting.setRace();
+        monsters = setting.getMonsters();
+        result.contentOfGame(startRacing(setting.getCount()));
+        result.resultOfGame(calculator.calculateResults(monsters));
+        setting.gameClose();
+    }
+
+    public ArrayList<Monster> startRacing(int count) {
+        System.out.println(Message.START);
         monsters.forEach(monster -> monster.run(count));
-        output.contentOfGame(monsters);
-        calculateResults();
-        output.resultOfGame(winnerMonsters);
-        input.close();
-    }
-
-    public void setting() {
-        while (true){
-            try{
-                monster = input.inputMonster();
-                count = input.inputCount();
-                makeMonster();
-                break;
-            } catch (NumberFormatException | InputMismatchException n){
-                System.out.println("숫자를 입력하세요.");
-            }
-        }
-    }
-
-    public void makeMonster(){
-        Monster newMonster;
-
-        message.monsterInfo();
-        for (int i = 0; i < monster; i++) {
-            newMonster = input.inputMonsterInfo();
-            monsters.add(newMonster);
-        }
-    }
-
-    public void calculateResults(){
-        int highRecord = calculateHighRecorde();
-
-        monsters.forEach(monster -> {
-            addWinnerMonster(monster, highRecord);
-        });
-    }
-
-    public void addWinnerMonster(Monster monster, int highRecord){
-        if(monster.totalMove.length() >= highRecord){
-            winnerMonsters.add(monster);
-        }
-    }
-
-    public int calculateHighRecorde(){
-        int record = 0;
-
-        for(int i=0; i<monsters.size(); i++){
-            Monster monster = monsters.get(i);
-            record = Math.max(record, monster.totalMove.length());
-        }
-
-        return record;
+        return monsters;
     }
 
 }
