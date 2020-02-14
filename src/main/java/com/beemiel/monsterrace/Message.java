038b7c5f692c5c14a4c996e4fe1598a0a62d2127
beemiel <qnswwrn@gmail.com>
2020-02-14 16:25:19 +0900

    Message<수정>: 메서드들을 없애고 상수들 집합으로 변경
    기존에 있던 출력메서드들을 없애고 문자열 상수들로만 수정

--- src/main/java/com/beemiel/monsterrace/Message.java
+++ src/main/java/com/beemiel/monsterrace/Message.java
@@ -2,40 +2,16 @@ package com.beemiel.monsterrace;
 
 public class Message {
 
-    final String TITLE = "<MONSTER RACE>";
-    final String START = "\n레이스 시작";
-    final String MONSTER = "몬스터는 모두 몇 마리인가요?";
-    final String MONSTER_INFO = "\n경주할 몬스터 이름과 종류를 입력하세요 (예> 크롱,FLY)";
-    final String TRY = "\n시도할 회수는 몇 회 인가요?";
-    final String RESULT = "\n<결  과>";
-    final String CLOSE = "\n<경기 종료>";
+    static final String TITLE = "<MONSTER RACE>";
+    static final String START = "\n레이스 시작";
+    static final String MONSTER_NUMBER = "몬스터는 모두 몇 마리인가요?";
+    static final String MONSTER_INFO = "\n경주할 몬스터 이름과 종류를 입력하세요. (예> 크롱,비행)";
+    static final String TRY = "\n시도할 회수는 몇 회 인가요?";
+    static final String RESULT = "\n<결  과>";
+    static final String CLOSE = "\n<경기 종료>";
 
-    public void gameTitle(){
-        System.out.println(TITLE);
-    }
-
-    public void gameStart(){
-        System.out.println(START);
-    }
-
-    public void getMonster(){
-        System.out.println(MONSTER);
-    }
-
-    public void monsterInfo(){
-        System.out.println(MONSTER_INFO);
-    }
-
-    public void getTry(){
-        System.out.println(TRY);
-    }
-
-    public void result(){
-        System.out.println(RESULT);
-    }
-
-    public void gameClose(){
-        System.out.println(CLOSE);
-    }
+    static final String EXCEPTION_OF_NUMBER = "숫자를 입력하세요!";
+    static final String EXCEPTION_OF_NULL = "형식을 지켜주세요......";
+    static final String EXCEPTION_OF_NOT_FOUND_TYPE = "존재하는 타입이 아닙니다... 비행, 달리기, 에스퍼 중에서 골라주세요..";
 
 }
