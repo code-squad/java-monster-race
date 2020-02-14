92812c83147b988c9e48724f8414450bdb1ca66b
beemiel <qnswwrn@gmail.com>
2020-02-14 16:18:00 +0900

    Input<수정>: 기존의 메서드들을 수정
    모든 메서드에서 입력뿐만 아니라 출력까지 수행하는 메서드들에서
    출력부분을
    제거하고 입력받은 값만 반환하도록 수정

--- src/main/java/com/beemiel/monsterrace/Input.java
+++ src/main/java/com/beemiel/monsterrace/Input.java
@@ -1,39 +1,20 @@
 package com.beemiel.monsterrace;
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Input {
     private Scanner scanner = new Scanner(System.in);
-    private Message message = new Message();
 
-    public int inputMonster() throws NumberFormatException{
-        message.gameTitle();
-        message.getMonster();
-        return Integer.parseInt(scanner.nextLine());
+    public String inputMonster() {
+        return scanner.nextLine();
     }
 
-    public Monster inputMonsterInfo() {
-        String[] info;
-
-        while(true){
-            try{
-                info = scanner.nextLine().split(",");
-                String type = info[1];
-                String name = info[0];
-                MonsterType monsterType = MonsterType.valueOf(type);
-                monsterType.getMonster().setName(name);
-                return monsterType.getMonster();
-            } catch (Exception e){
-                e.getMessage();
-                System.out.println("제대로 된 타입을 입력하세요");
-            }
-        }
+    public String inputMonsterInfo(){
+        return scanner.nextLine();
     }
 
-    public int inputCount() throws InputMismatchException {
-        message.getTry();
-        return scanner.nextInt();
+    public String inputCount() {
+        return scanner.nextLine();
     }
 
     public void close(){
