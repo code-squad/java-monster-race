e823694bd871615754826f664f737324ba4287f8
beemiel <qnswwrn@gmail.com>
2020-02-14 16:23:27 +0900

    Result<수정>: Output클래스를 Result 클래스로 변경
    contentOfGame() - 게임 결과를 출력한다
    appendString() - 우승자들의 이름을 하나의 문자열로 붙여서 반환한다
    resultOfGame() - 우승사를 출력한다

--- src/main/java/com/beemiel/monsterrace/Result.java
+++ src/main/java/com/beemiel/monsterrace/Result.java
@@ -3,22 +3,24 @@ package com.beemiel.monsterrace;
 import java.util.ArrayList;
 import java.util.StringJoiner;
 
-public class Output {
-    private Message message = new Message();
+public class Result {
 
     public void contentOfGame(ArrayList<Monster> monsters){
-        message.gameStart();
-        message.result();
         monsters.forEach(System.out::println);
     }
 
-    public void resultOfGame(ArrayList<Monster> winners){
+    public String appendString(ArrayList<Monster> winners) {
         StringJoiner stringJoiner = new StringJoiner(",","[","]");
         for(Monster monster : winners){
             stringJoiner.add(monster.getName());
         }
-        System.out.println("축하합니다~ " + stringJoiner.toString() + " 가 레이스에서 우승하였습니다.");
-        message.gameClose();
+        return stringJoiner.toString();
+    }
+
+    public void resultOfGame(ArrayList<Monster> winners){
+        System.out.println(Message.RESULT);
+        System.out.println("축하합니다~ " + appendString(winners) + " 가 레이스에서 우승하였습니다.");
+        System.out.println(Message.CLOSE);
     }
 
 }
